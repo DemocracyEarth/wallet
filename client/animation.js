11dbac8670fcca1e58dd1fa49b4fd25823faaaf7
Santiago Siri <santisiri@gmail.com>
2016-04-22 18:37:01 -0700

    improved animation implementation

--- client/animation.js
+++ client/animation.js
@@ -6,39 +6,46 @@ if (Meteor.isClient) {
     //Animation states
     var init = {
      'opacity': '0',
+     //'margin': '5px 0',
+     //'position': 'absolute',
      'overflow': 'hidden',
      'height' : '0px'
     };
     var finish = {
-      'opacity': 1,
-      'height': '35px'
+      'opacity': '1',
+      'height': '36px'
     };
+    var exit = {
+      'opacity': '0',
+      'height': '0px'
+    }
 
     //Configure UI hooks
     var hooks = {
       insertElement: function(node, next) {
         $(node).css(init);
         $(node).insertBefore(next);
-        $(node).velocity(finish, ANIMATION_DURATION);
-
+        $(node).velocity(finish, {
+          duration: ANIMATION_DURATION,
+          //easing: 'ease-in',
+          queue: false
+        });
         Deps.afterFlush(function() {
           $(node).width();
         });
       },
       removeElement: function(node) {
-        console.log('REMOVE: ' + node);
-        var item = $(node);
         $(node)
-          .velocity({translateX: 20}, {
+          .velocity(exit, {
             duration: ANIMATION_DURATION,
-            easing: 'ease-in-out',
+            //easing: 'ease-out',
             queue: false,
             complete: function() {
               $(node).remove();
             }
           });
       }
-    }
+    };
 
     //Animate object
     if (main.parentNode != null) {
