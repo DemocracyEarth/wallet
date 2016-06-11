d72de8afca8368ec6cfaa28f90af9dc89cdd4f8c
Santiago Siri <santisiri@gmail.com>
2016-06-11 01:09:12 -0700

    css style for modals

---
+++ client/components/decision/signatures/signatures.js
@@ -0,0 +1,48 @@
+Template.signatures.rendered = function () {
+  var contractAuthors = Contracts.findOne({ _id: Session.get('contractId') }).authors;
+
+  if (contractAuthors != undefined) {
+    for (var i = 0; i < contractAuthors.length; i++ ) {
+      if (Meteor.user() != null) {
+        if (contractAuthors[i]._id == Meteor.user()._id) {
+          Session.set('userSigned', true);
+        } else {
+          Session.set('userSigned', false);
+        }
+      }
+    }
+  }
+
+  Session.set('displaySignaturePopup', false);
+
+};
+
+Template.signatures.helpers({
+  userSigned: function () {
+    return Session.get('userSigned');
+  },
+  toggle: function (picture) {
+    if (Session.get('displaySignaturePopup')) {
+      if (picture) {
+        return '-white.png';
+      } else {
+        return 'profile-sign';
+      }
+    } else {
+      if (picture) {
+        return '.png';
+      } else {
+        return '';
+      }
+    }
+  },
+  displaySignaturePopup: function () {
+    return Session.get('displaySignaturePopup');
+  }
+});
+
+Template.signatures.events({
+  'click #signature': function () {
+    Session.set('displaySignaturePopup', !Session.get('displaySignaturePopup'))
+  }
+})
