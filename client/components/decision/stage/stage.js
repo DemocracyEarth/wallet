42aa9e7fca44778574e9a4d4f1d09f7cb404417e
Santiago Siri <santisiri@gmail.com>
2016-06-24 17:42:15 -0700

    signal component for contract stage status

---
+++ client/components/decision/stage/stage.js
@@ -0,0 +1,36 @@
+Template.stage.helpers({
+  text: function () {
+    switch(this.text) {
+      case 'DRAFT':
+        return TAPi18n.__('kind-draft-vote');
+      case 'LIVE':
+        return TAPi18n.__('kind-live-vote');
+      case 'FINISH':
+        switch(this.executionStatus) {
+          case 'APPROVED':
+            return TAPi18n.__('kind-finish-vote-approved');
+          case 'ALTERNATIVE':
+            return TAPi18n.__('kind-finish-vote-alternative');
+          case 'REJECTED':
+            return TAPi18n.__('kind-finish-vote-rejected');
+        }
+    }
+  },
+  style: function (text, executionStatus) {
+    switch (text) {
+      case 'DRAFT':
+        return 'stage-draft';
+      case 'LIVE':
+        return 'stage-live';
+      case 'FINISH':
+        switch(executionStatus) {
+          case 'APPROVED':
+            return 'stage-finish-approved';
+          case 'ALTERNATIVE':
+            return 'stage-finish-alternative';
+          case 'REJECTED':
+            return 'stage-finish-rejected';
+        }
+    }
+  }
+})
