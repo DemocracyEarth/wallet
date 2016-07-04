fc48d20d6dfe2580f1ef067ca7a1ee21978ebc67
Santiago Siri <santisiri@gmail.com>
2016-07-03 19:50:19 -0700

    hover icon resolved for feed button

--- client/components/widgets/feed/feedButton.js
+++ client/components/widgets/feed/feedButton.js
@@ -1,18 +1,21 @@
 Template.feedButton.rendered = function () {
-  Session.set('feedToggle', false);
-  console.log(this.id);
+  //feedButtonId.push(this.firstNode.id);
+  //Session.set('feedToggle' + feedButtonId, false);
 };
 
 Template.feedButton.helpers({
-  mouseDown: function () {
-    return Session.get('feedToggle');
+  display: function () {
+
+    //return Session.get('feedToggle' + feedButtonId);
   }
 })
 Template.feedButton.events({
-  'mousedown #button': function () {
-    Session.set('feedToggle', true);
+  'mousedown .feedButton': function (event) {
+    var buttonId = 'button-' + event.target.id;
+    document.getElementById(buttonId).style.opacity = 0;
   },
-  'mouseup #button': function () {
-    Session.set('feedToggle', false);
+  'mouseup .feedButton': function (event) {
+    var buttonId = 'button-' + event.target.id;
+    document.getElementById(buttonId).style.opacity = 1;
   }
 })
