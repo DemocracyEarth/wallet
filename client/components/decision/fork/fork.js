6f5de95df23d8cbc340d06b3ad3cc2354769e40c
Santiago Siri <santisiri@gmail.com>
2016-07-16 23:22:47 -0700

    calibrated dynamic font size for ballot button

--- client/components/decision/fork/fork.js
+++ client/components/decision/fork/fork.js
@@ -1,8 +1,8 @@
 Template.fork.helpers({
   length: function () {
-    if (this.label.length > 40) {
+    if (this.label.length > 47) {
       return 'option-link option-long option-longest';
-    } else if (this.label.length > 25) {
+    } else if (this.label.length > 36) {
       return 'option-link option-long';
     } else {
       return '';
