4ad20cf0d32fae5c128882bf684d9f6cf4b86078
Eduardo Medina <medied@Eduardos-MacBook-Pro.local>
2016-08-17 21:47:57 -0700

    Implemented call to Passwords API

--- client/components/identity/login/forgotPassword.js
+++ client/components/identity/login/forgotPassword.js
@@ -1,13 +1,26 @@
-Template.forgotPassword.rendered = function () {
-  var enableLogin = false;
+Template.forgotPassword.rendered = function () {}
+// Template.forgotPassword.helpers({})
+Template.forgotPassword.events({
+  "click #recovery-button": function (event){
+    console.log("onClick: recovery-button");
+    
+    //get recovery email
+    var email = document.getElementById('recovery-email').value;
+    
+    //validate non-empty email & invoke Passwords API
+    if (email != '') {
+      Accounts.forgotPassword({email: email}, function(err) {
+        if (err) {
+          if (err.message === 'User not found [403]') {
+            console.log('This email does not exist.');
+          } else {
+            console.log('We are sorry but something went wrong.');
+          }
+        } else {
+          console.log('Email Sent. Check your mailbox.');
+        }
+      });
+    }
+  }
+})
 
-  //Give everyone a chance to not fuckup
-  // Session.set("invalidUsername", false);
-  // Session.set("repeatedUsername", false);
-  // Session.set("invalidEmail", false);
-  // Session.set("invalidPassword", false);
-  // Session.set("shortPassword", false);
-  // Session.set("mismatchPassword", false);
-  // Session.set("alreadyRegistered", false);
-
-}
