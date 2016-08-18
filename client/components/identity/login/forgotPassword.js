808fc182bb1cda51ade3e8ebcd84173fcce1b5fa
Eduardo Medina <medied@Eduardos-MacBook-Pro.local>
2016-08-18 11:38:25 -0700

    Added warning popups for emails successfully sent and for errors

--- client/components/identity/login/forgotPassword.js
+++ client/components/identity/login/forgotPassword.js
@@ -1,26 +1,47 @@
-Template.forgotPassword.rendered = function () {}
-// Template.forgotPassword.helpers({})
+Template.forgotPassword.rendered = function () {
+  Session.set("emailSent", false);
+  Session.set("error1", false);
+  Session.set("error2", false);
+}
+
+Template.forgotPassword.helpers({
+  emailSent: function () {
+    return Session.get("emailSent");
+  },
+  error1: function () {
+    return Session.get("error1");
+  },
+  error2: function () {
+    return Session.get("error2");
+  }
+});
+
 Template.forgotPassword.events({
   "click #recovery-button": function (event){
     console.log("onClick: recovery-button");
     
-    //get recovery email
+    //Get recovery email
     var email = document.getElementById('recovery-email').value;
+
+    //Reset helpers in case #recovery-button is clicked multiple times
+    Session.set("emailSent", false);
+    Session.set("error1", false);
+    Session.set("error2", false);
     
-    //validate non-empty email & invoke Passwords API
+    //Validate non-empty email & invoke Passwords API
     if (email != '') {
       Accounts.forgotPassword({email: email}, function(err) {
         if (err) {
           if (err.message === 'User not found [403]') {
-            console.log('This email does not exist.');
+            Session.set("error1", true);
           } else {
-            console.log('We are sorry but something went wrong.');
+            Session.set("error2", true);
           }
         } else {
-          console.log('Email Sent. Check your mailbox.');
+          Session.set("emailSent", true);
         }
       });
     }
   }
-})
+});
 
