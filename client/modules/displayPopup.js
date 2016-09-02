16d4264e9de56d10b4172e007a84f518c5f2ea4b
Santiago Siri <santisiri@gmail.com>
2016-09-02 14:53:29 -0700

    popup pointer positioning relative to source element

--- client/modules/displayPopup.js
+++ client/modules/displayPopup.js
@@ -33,87 +33,50 @@ let _displayPopup = (content, sourceElement) => {
 
 
 let _positionCard = (sourceElement, target) => {
-  var pointerClass = new String;
+  var left = new Number();
+  var pointer = new Number();
   var source = sourceElement.getBoundingClientRect();
-  var centerPos = (source.width / 2);
-
-  //Y Axis
-  if (source.top < parseInt(target.height + 60)) {
-    //Place on bottom
-    //popupCard.position['margin-top'] = -5;
-
-    //popup goes at bottom of target
-    popupCard.position['top'] = source.bottom;
-    pointerClass = 'pointer-up';
-
-  } else {
-    //Place at top
-    //position['margin-top'] = parseInt(source.top + sourceElement.offsetHeight + 10);
-
-    //popup goes on top of target
-    popupCard.position['top'] = parseInt(source.top - target.height);
-    pointerClass = 'pointer-down';
-
-  }
-
-  //X Axis
   var spaceRight = parseInt(document.body.offsetWidth - source.right);
   var spaceLeft = parseInt(source.left);
   var documentHalf = parseInt(document.body.offsetWidth / 2);
 
-  console.log(sourceElement);
-  console.log(source.left);
-  console.log('spaceLeft = ' + spaceLeft);
+  //Y Axis
+  if (source.top < parseInt(target.height + 60)) {
+    //popup goes at bottom of target
+    popupCard.position['top'] = source.bottom;
+    popupCard.pointerClass = '.pointer-up';
+  } else {
+    //popup goes on top of target
+    popupCard.position['top'] = parseInt(source.top - target.height);
+    popupCard.pointerClass = '.pointer-down';
+  }
 
+  //X Axis
   if (source.left > documentHalf) {
     //popup will be on right side of screen
-
     if (spaceRight < (target.width - (target.width / 2))) {
       //not enough space on the right for Popup centering
-      console.log('not enough space on the right for Popup centering.');
       left = parseInt(source.left - target.width + source.width);
+      pointer = parseInt(target.width - (source.width / 2) - 10);
     } else {
       //enough space on the right, Popup is centered.
-      console.log('enough space on the right, Popup is centered.');
       left = parseInt(source.left - (target.width / 2) + (source.width / 2));
+      pointer = parseInt(target.width - (target.width / 2) - 10);
     }
   } else {
     //popup will be on left side of screen
     if (spaceLeft < (target.width - (target.width / 2))) {
       //not enough space on left
-      console.log('not enough space on left');
       left = parseInt(source.left);
+      pointer = parseInt((source.width / 2) - 10);
     } else {
       //enough space on left;
-      console.log('enough space on left');
       left = parseInt(source.left - (target.width / 2) + (source.width / 2));
+      pointer = parseInt(target.width - (target.width / 2) - 10);
     }
   }
-
   popupCard.position['left'] = left;
-
-  //Right side of screen
-/*  if (source.left > (document.body.offsetWidth / 2)) {
-    if (spaceRight < (target.width - (target.width / 2))) {
-      //little space on the right
-      var newMargin = (0 - target.width + spaceRight + source.width - 10);
-      if (newMargin <= 0) {
-        popupCard.position['margin-left'] = newMargin;
-      } else {
-        popupCard.position['margin-left'] = 0;
-      }
-      popupCard.cursorLeft = target.width - spaceRight - (source.width / 2) + 5;
-    } else {
-      //centered
-      popupCard.position['margin-left'] = (0 - (target.width / 2) +  (source.width / 2) + 5);
-      popupCard.cursorLeft = (target.width / 2) - 10;
-    }
-  } else {
-    //Left side of screen-new-proposal
-
-    //TODO
-
-  }*/
+  popupCard.pointerPosition = pointer;
 
   return Object.assign(popupCard.position, target);
 }
@@ -121,7 +84,7 @@ let _positionCard = (sourceElement, target) => {
 let _renderPopup = () => {
 
   //Pointer
-  $('.pointer-up').css({ 'left' : popupCard.cursorLeft });
+  $('.pointer-up').css({ 'left' : popupCard.pointerPosition });
 
   //positioning
   $('#popup').css(popupCard.position);
