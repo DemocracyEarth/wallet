5344d8f66dbbdf3d58ed5d50134ae953df949032
lucasisasmendi <lucas.isasmendi@gmail.com>
2016-11-05 00:23:45 -0300

    fix import

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -16,15 +16,21 @@ Buckminster Fuller, Great San Francisco Architect.
 A Roma, <3
 
 */
-
 import { Meteor } from 'meteor/meteor';
+import Collectives from '../../api/collectives/Collectives';
 
-//This script runs the first time Democracy Earth gets implemented on a server.
+
+// This script runs the first time Democracy Earth gets implemented on a server.
 console.log(Meteor.settings.public.app.name + ' version: ' + Meteor.settings.public.app.version + ' codename: ' + Meteor.settings.public.app.codename );
 
-Meteor.startup(function () {
-  //App Cache
-  //
+function setupCollectiveInApp(collective) {
+  Meteor.settings.public.Collective._id = collective._id;
+  console.log('This server is being run by collective ' + collective.name);
+  console.log('The collective can be reached with id ' + collective._id);
+}
+
+Meteor.startup(() => {
+  // App Cache
   /* appcache issue with browser test: infinite loop
   console.log('[startup] app cache: ' + Meteor.settings.public.app.config.appCache)
   Meteor.AppCache.config({
@@ -32,32 +38,26 @@ Meteor.startup(function () {
     firefox: Meteor.settings.public.app.config.appCache,
     safari: Meteor.settings.public.app.config.appCache
   });
-  */  
+  */
   console.log('Verifiying main Collective in server...');
-  var dbCollective = Collectives.findOne({ domain: Meteor.settings.public.Collective.domain });
+  const dbCollective = Collectives.findOne({ domain: Meteor.settings.public.Collective.domain });
 
   if (dbCollective) {
-      setupCollectiveInApp(dbCollective);
+    setupCollectiveInApp(dbCollective);
   } else {
-    //Organization has no Collective in db yet
+    // Organization has no Collective in db yet
     console.log('Installing democracy in server...');
-    if (Meteor.settings.public.Collective == undefined) {
+    if (Meteor.settings.public.Collective === undefined) {
       console.log('-- MISSING SETTING: Collective was not found. Please setup settings.json on config/development')
     } else {
       console.log('Setting up main Collective in database...');
-      Collectives.insert(Meteor.settings.public.Collective, function( error, result) {
-          if ( error ) console.log ( "-- ERROR: Could not add main collective to database. " + error );
-          if ( result ) {
-            console.log ( "SUCCESS: Main collective set up with id " + result );
-            setupCollectiveInApp( Collectives.findOne({ domain: Meteor.settings.public.Collective.domain }));
-          }
+      Collectives.insert(Meteor.settings.public.Collective, function(error, result) {
+        if (error) console.log ( "-- ERROR: Could not add main collective to database. " + error );
+        if (result) {
+          console.log ( "SUCCESS: Main collective set up with id " + result );
+          setupCollectiveInApp(Collectives.findOne({ domain: Meteor.settings.public.Collective.domain }));
         }
-      );
+      });
     }
-  } 
+  }
 });
-function setupCollectiveInApp (collective) {
-  Meteor.settings.public.Collective._id = collective._id;
-  console.log('This server is being run by collective ' + collective.name);
-  console.log('The collective can be reached with id ' + collective._id);
-}
