437c3a19d9b2ddf7b5158213a0d6d18347a77710
lucasisasmendi <lucas.isasmendi@gmail.com>
2016-11-07 01:49:30 -0300

    added imports, rp Modules.both

--- imports/ui/templates/components/decision/signatures/signatures.js
+++ imports/ui/templates/components/decision/signatures/signatures.js
@@ -1,5 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
+import { TAPi18n } from 'meteor/tap:i18n';
+
+import { getAnonymous } from '/imports/startup/both/modules/User';
+import { signContract } from '/imports/startup/both/modules/Contract';
+
 Template.signatures.rendered = function () {
-  if (!Session.get('contract')) { return };
+  if (!Session.get('contract')) { return; }
   var contractAuthors = Session.get('contract').signatures;
   if (contractAuthors != undefined) {
     for (var i = 0; i < contractAuthors.length; i++ ) {
@@ -31,7 +39,7 @@ Template.signatures.helpers({
       } else {
         //is anonymous
         if (!this.editorMode) {
-          return [Modules.both.getAnonymous(true)];
+          return [getAnonymous(true)];
         }
       }
     }
@@ -43,17 +51,17 @@ Template.signatures.events({
     Modules.client.displayModal(
       true,
       {
-        icon            : 'images/author-signature.png',
-        title           : TAPi18n.__('proposal-author'),
-        message         : TAPi18n.__('proposal-signed-identity'),
-        cancel          : TAPi18n.__('not-now'),
-        action          : TAPi18n.__('sign-proposal'),
-        displayProfile  : true,
-        profileId       : Meteor.user()._id
+        icon: 'images/author-signature.png',
+        title: TAPi18n.__('proposal-author'),
+        message: TAPi18n.__('proposal-signed-identity'),
+        cancel: TAPi18n.__('not-now'),
+        action: TAPi18n.__('sign-proposal'),
+        displayProfile: true,
+        profileId: Meteor.user()._id
       },
       function() {
         Session.set('userSigned', true);
-        Modules.both.signContract(Session.get('contract')._id, Meteor.user(), 'AUTHOR');
+        signContract(Session.get('contract')._id, Meteor.user(), 'AUTHOR');
       }
     );
   }
