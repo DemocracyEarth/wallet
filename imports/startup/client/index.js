005a3229d7741cf7da3c709c3c2d54154d7ea9b8
lucasisasmendi <lucas.isasmendi@gmail.com>
2016-11-07 21:03:03 -0300

    import css files

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,1 +1,9 @@
+import '/imports/ui/css/lib/normalize.css';
+import '/imports/ui/css/lib/webflow.css';
+
+import '/imports/ui/css/datepicker.css';
+import '/imports/ui/css/earth.css';
+import '/imports/ui/css/extra.css';
+import '/imports/ui/css/medium-editor-insert-plugin.css';
+import '/imports/ui/css/medium-editor.css';
 import './routes.js';
