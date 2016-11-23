69022e33a6773fecd229f08e8a6f632be0774ed7
makgoa <cawleynm@gmail.com>
2016-11-23 16:57:06 -0500

    chore:(test-utils) add fake document generators add fake json document generator for each schema in /imports/api/* add sanity checks to ensure fake documents validate

---
+++ imports/utils/test-utils/faker-schema/Ballot.js
@@ -0,0 +1,25 @@
+export const Ballot = {
+  type: 'object',
+  properties: {
+    _id: {
+      type: 'string'
+    },
+    mode: {
+      type: 'string'
+    },
+    rank: {
+      type: 'number'
+    },
+    url: {
+      type: 'string',
+      faker: 'internet.url'
+    },
+    label: {
+      type: 'string'
+    }
+  },
+  required: [
+    '_id',
+    'mode'
+  ]
+}
---
+++ imports/utils/test-utils/faker-schema/User.js
@@ -0,0 +1,136 @@
+import { Wallet } from './Wallet';
+import { Country } from './Collective';
+
+export const Credential = {
+  type: 'object',
+  properties: {
+    source: {
+      enum: ['facebook', 'twitter', 'linkedin', 'github', 'peer'],
+    },
+    URL: {
+      faker: 'internet.url'
+    },
+    validated: {
+      type: 'boolean'
+    }
+  }
+}
+
+export const Menu = {
+  type: 'object',
+  properties: {
+    feed: 'all',
+    lastView: {
+      faker: 'date.past'
+    },
+    newItems: {
+      type: 'number',
+      minimum: 0
+    }
+  }
+}
+
+export const Profile = {
+  type: 'object',
+  properties: {
+    firstName: {
+      faker: 'name.firstName',
+    },
+    lastName: {
+      faker: 'name.lastName'
+    },
+    picture: {
+      faker: 'image.imageUrl'
+    },
+    country: Country,
+    birthday: {
+      faker: 'date.past'
+    },
+    gender: {
+      enum: ['Male', 'Female']
+    },
+    organization : {
+      faker: 'company.companyName'
+    },
+    website: {
+      faker: 'internet.url'
+    },
+    bio: {
+      type: 'string'
+    },
+    configured: {
+      type: 'boolean',
+    },
+    credentials: {
+      type: 'array',
+      items: {
+        type: Credential
+      }
+    },
+    url: {
+      faker: 'internel.url'
+    },
+    menu: {
+      type: 'array',
+      items: {
+        type: Menu
+      }
+    },
+    wallet: Wallet
+  }
+}
+
+export const User = {
+  type: 'object',
+  properties: {
+    username: {
+      faker: 'internet.userName'
+    },
+    emails: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          address: {
+            faker: 'internet.email'
+          },
+          verified: {
+            type: 'boolean'
+          }
+        },
+        required: [
+          'address',
+          'verified'
+        ]
+      }
+    },
+    registered_emails: {
+      type: 'array',
+      items: {
+        type: 'object'
+      }
+    },
+    createdAt: {
+      faker: 'date.past'
+    },
+    profile: {
+      type: Profile
+    },
+    services: {
+      type: 'object'
+    },
+    roles: {
+      type: 'object'
+    },
+    roles: {
+      type: 'array',
+      items: {
+        type: 'string'
+      }
+    },
+    heartbeat: {
+      faker: 'date.past'
+    }
+  },
+  required: ['createdAt']
+}
