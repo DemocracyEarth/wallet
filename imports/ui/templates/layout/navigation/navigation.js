aba05123055ebee1a99937a2befea492206eedb4
Santiago Siri <santisiri@gmail.com>
2016-12-09 16:19:27 -0200

    navigation went through linter

--- imports/ui/templates/layout/navigation/navigation.js
+++ imports/ui/templates/layout/navigation/navigation.js
@@ -13,70 +13,67 @@ import './navigation.html';
 import '../authentication/authentication.js';
 import '../../widgets/notice/notice.js';
 
-//Scroll behaviour
-var lastScrollTop = 0;
-var scrollDown = false;
+// Scroll behaviour
+let lastScrollTop = 0;
+let scrollDown = false;
 
 if (Meteor.Device.isPhone()) {
-  $(window).scroll(function(event) {
+  $(window).scroll(function () {
     const node = $('.navbar');
     const st = $(this).scrollTop();
     if (st > lastScrollTop) {
       if (scrollDown === false && st > 150) {
         scrollDown = true;
-        animate(node, 'hide-up', { duration: parseInt(timers.ANIMATION_DURATION * 2.5), easing: 'ease-in' });
+        animate(node, 'hide-up', { duration: parseInt(timers.ANIMATION_DURATION * 2.5, 10), easing: 'ease-in' });
       }
-    } else {
-      if (scrollDown === true) {
-        scrollDown = false;
-        animate(node, 'show-down', { duration: parseInt(timers.ANIMATION_DURATION * 2.5), easing: 'ease-out' });
-      }
+    } else if (scrollDown === true) {
+      scrollDown = false;
+      animate(node, 'show-down', { duration: parseInt(timers.ANIMATION_DURATION * 2.5, 10), easing: 'ease-out' });
     }
     lastScrollTop = st;
   });
+}
+
+function displayMenuIcon() {
+  if (Session.get('sidebar')) {
+    return 'images/burger-active.png';
+  }
+  return 'images/burger.png';
 }
 
 Template.navigation.rendered = function rendered() {
 };
 
 Template.navigation.helpers({
-  screen: function () {
+  screen() {
     if (Session.get('navbar')) {
-      document.title = stripHTMLfromText(TAPi18n.__('democracy-of') + ' ' + Meteor.settings.public.Collective.name + ' - ' + Session.get('navbar').title);
+      document.title = stripHTMLfromText(`${TAPi18n.__('democracy-of')} ${Meteor.settings.public.Collective.name} - ${Session.get('navbar').title}`);
       return Session.get('navbar').title;
-    } else {
-      document.title = stripHTMLfromText(TAPi18n.__('democracy-earth'));
     }
+    document.title = stripHTMLfromText(TAPi18n.__('democracy-earth'));
+    return '';
   },
-  icon: function () {
-    if (Session.get('navbar') != undefined) {
+  icon() {
+    if (Session.get('navbar') !== undefined) {
       return displayMenuIcon();
-    } else {
-      return 'images/burger.png';
     }
+    return 'images/burger.png';
   },
-  link: function () {
+  link() {
     if (Session.get('navbar')) {
       return Session.get('navbar').href;
     }
+    return '';
   },
-  showNotice: function () {
+  showNotice() {
     return Session.get('showNotice');
-  }
+  },
 });
 
 Template.navigation.events({
-  "click #menu": function (event) {
-    if (Session.get('navbar').action == 'SIDEBAR') {
+  'click #menu'() {
+    if (Session.get('navbar').action === 'SIDEBAR') {
       toggleSidebar();
     }
-  }
-})
-
-function displayMenuIcon() {
-  if (Session.get('sidebar')) {
-    return 'images/burger-active.png';
-  } else {
-    return 'images/burger.png';
-  }
-}
+  },
+});
