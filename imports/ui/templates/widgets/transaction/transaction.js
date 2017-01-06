790fde18a28592d73cc7a301e41272eba0dd0d28
Santiago Siri <santisiri@gmail.com>
2017-01-05 23:26:58 -0800

    polished html template for transaction, helper code schema included

--- imports/ui/templates/widgets/transaction/transaction.js
+++ imports/ui/templates/widgets/transaction/transaction.js
@@ -5,3 +5,22 @@ import { Router } from 'meteor/iron:router';
 import { TAPi18n } from 'meteor/tap:i18n';
 
 import './transaction.html';
+
+Template.transaction.onRendered = function (value) {
+  console.log(value);
+};
+
+Template.transaction.helpers({
+  sender() {
+
+  },
+  receiver() {
+
+  },
+  value() {
+    return '393';
+  },
+  source() {
+
+  },
+});
