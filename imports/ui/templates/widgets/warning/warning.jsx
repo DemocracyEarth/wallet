8185eb4d45d3bcfc123635e16a2024b41a7bf268
Eduardo Medina <medina.eag@gmail.com>
2017-01-23 11:20:21 -0800

    Implemented warning widget as a React component

---
+++ imports/ui/templates/widgets/warning/warning.jsx
@@ -0,0 +1,22 @@
+import React, { Component, PropTypes } from 'react';
+import { behave } from '/imports/ui/modules/animation';
+import { TAPi18n } from 'meteor/tap:i18n';
+
+export default class Warning extends Component {
+  componentDidMount() {
+    behave(this.props.warning, 'fade-and-roll', { height: '36px' });
+  }
+
+  render() {
+    return (
+      <ul className="w-list-unstyled warning-list animate">
+        <li className="warning">{TAPi18n.__(this.props.label)}</li>
+      </ul>
+    );
+  }
+}
+
+Warning.PropTypes = {
+  warning: PropTypes.object.isRequired,
+  label: PropTypes.string.isRequired,
+};
