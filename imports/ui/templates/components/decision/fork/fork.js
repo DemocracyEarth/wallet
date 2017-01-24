d86859f2e4aa1b8226be4ec9166529c535a79b49
Santiago Siri <santisiri@gmail.com>
2017-01-24 00:03:44 -0800

    linter on forks

--- imports/ui/templates/components/decision/fork/fork.js
+++ imports/ui/templates/components/decision/fork/fork.js
@@ -7,60 +7,59 @@ import { setVote, getVote } from '/imports/ui/modules/ballot';
 import './fork.html';
 
 Template.fork.helpers({
-  length: function () {
+  length() {
     if (this.label !== undefined) {
       if (this.label.length > 51) {
         return 'option-link option-long option-longest';
       } else if (this.label.length > 37) {
         return 'option-link option-long';
-      } else {
-        return '';
       }
-    };
+    }
     return '';
   },
-  dragMode: function () {
+  dragMode() {
     if (Session.get('contract').stage === 'DRAFT') {
       return '';
-    } else {
-      return 'vote-nondrag';
     }
+    return 'vote-nondrag';
   },
-  tickStyle: function () {
+  tickStyle() {
     if (this.mode === 'REJECT') {
       return 'unauthorized';
     }
+    return '';
   },
-  checkbox: function (mode) {
+  checkbox(mode) {
     switch (mode) {
       case 'AUTHORIZE':
         return 'vote-authorize nondraggable';
       case 'REJECT':
         return 'vote-authorize unauthorized nondraggable';
       case 'FORK':
+      default:
         return 'vote vote-alternative';
     }
   },
-  action: function () {
-      if (this.authorized == false) {
-        return 'undefined';
-      }
+  action() {
+    if (this.authorized === false) {
+      return 'undefined';
+    }
+    return '';
   },
-  option: function (mode) {
-    if (Session.get('contract').stage == 'DRAFT' || ( Session.get('rightToVote') == false && Session.get('contract').stage != 'DRAFT' )) {
+  option(mode) {
+    if (Session.get('contract').stage === 'DRAFT' || (Session.get('rightToVote') === false && Session.get('contract').stage !== 'DRAFT')) {
       return 'disabled';
-    } else {
-      switch (mode) {
-        case 'AUTHORIZE':
-          return '';
-        case 'REJECT':
-          return 'option-link';
-        default:
-          return '';
-      }
+    }
+    switch (mode) {
+      case 'AUTHORIZE':
+        return '';
+      case 'REJECT':
+        return 'option-link';
+      default:
+        return '';
     }
   },
-  decision: function (mode) {
+  decision(mode) {
     switch (mode) {
       case 'REJECT':
         return 'option-link unauthorized';
@@ -68,36 +67,42 @@ Template.fork.helpers({
         return '';
     }
   },
-  caption: function (mode) {
-    if (mode != 'FORK') {
+  caption(mode) {
+    if (mode !== 'FORK') {
       return TAPi18n.__(mode);
-    } else {
-      return this.label;
     }
+    return this.label;
   },
-  tick: function () {
-    if (Session.get('contract').stage == 'DRAFT') {
-      return 'disabled'
+  tick() {
+    if (Session.get('contract').stage === 'DRAFT') {
+      return 'disabled';
     }
+    return '';
   },
-  tickStatus: function () {
+  tickStatus() {
     this.tick = getVote(Session.get('contract')._id, this._id);
     if (Session.get('candidateBallot')) {
       if (this.tick) {
         if (this.mode === 'REJECT') {
           return 'tick-active-unauthorized';
-        } else {
-          return 'tick-active';
         }
+        return 'tick-active';
       }
     } else if (Session.get('rightToVote') === false && Session.get('contract').stage !== 'DRAFT') {
-      //already voted
+      // already voted
       if (this.tick) {
-        return 'tick-disabled'
+        return 'tick-disabled';
       }
     }
     return '';
-  }
+  },
+  style(className) {
+    let final = className;
+    if (this.mini === true) {
+      final += ` ${final}-mini`;
+    }
+    return final;
+  },
 });
 
 
