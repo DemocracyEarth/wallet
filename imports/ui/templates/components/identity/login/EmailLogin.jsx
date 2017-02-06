b64739df1c0c20f1440c82b7dc6e41f11bc63522
Eduardo Medina <medina.eag@gmail.com>
2017-02-06 13:21:26 -0800

    Handles submit logic to login

--- imports/ui/templates/components/identity/login/EmailLogin.jsx
+++ imports/ui/templates/components/identity/login/EmailLogin.jsx
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import { TAPi18n } from 'meteor/tap:i18n';
 import Warning from '../../../widgets/warning/Warning.jsx';
@@ -10,10 +11,14 @@ export default class EmailLogin extends Component {
     this.state = {
       loginScreen: true,
       passwordKnown: true,
+      incorrectUser: false,
     };
 
     this.handleLoginRender = this.handleLoginRender.bind(this);
     this.handleForgotPasswordRender = this.handleForgotPasswordRender.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSigninError = this.handleSigninError.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
   }
 
   handleLoginRender() {
@@ -24,24 +29,57 @@ export default class EmailLogin extends Component {
     this.setState({ passwordKnown: !(this.state.passwordKnown) });
   }
 
+  handleFocus() {
+    this.setState({ incorrectUser: false });
+  }
+
+  handleSigninError() {
+    this.setState({ incorrectUser: true });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('signin-email').value;
+    const pass = document.getElementById('signin-password').value;
+
+    Meteor.loginWithPassword(email, pass, (error) => {
+      if (error) {
+        switch (error.error) {
+          case 400:
+            // Blank email and/or password
+            this.handleSigninError();
+            break;
+          case 403:
+            // User not found or incorrect password
+            this.handleSigninError();
+            break;
+        }
+      }
+    });
+  }
+
   render() {
+    const incorrectUserState = this.state.incorrectUser;
+
     if (this.state.loginScreen === true) {
       if (this.state.passwordKnown === true) {
         return (
           <div className="login">
             <div className="w-form">
-              <form id="email-signin-form" name="email-form-3" data-name="Email Form 3">
+              <form id="email-signin-form" name="email-form-3" data-name="Email Form 3" onSubmit={this.handleSubmit}>
                 <div className="w-clearfix login-field">
                   <label htmlFor="name" className="login-label login-label-form">{TAPi18n.__('email-username')}</label>
                   <img src="/images/mail-closed.png" className="login-icon" alt="mail-closed" />
-                  <input id="signin-email" type="text" placeholder={TAPi18n.__('email-sample')} className="w-input login-input" />
+                  <input id="signin-email" type="text" placeholder={TAPi18n.__('email-sample')} className="w-input login-input" onFocus={this.handleFocus} />
                 </div>
                 <div className="w-clearfix login-field">
                   <label htmlFor="name" className="login-label login-label-form">{TAPi18n.__('password')}</label>
                   <img src="/images/lock.png" className="login-icon" alt="lock" />
-                  <input id="signin-password" type="password" placeholder={TAPi18n.__('password-sample')} className="w-input login-input" />
+                  <input id="signin-password" type="password" placeholder={TAPi18n.__('password-sample')} className="w-input login-input" onFocus={this.handleFocus} />
                 </div>
-                <button type="submit" id="signin-button" className="button login-button">
+                {incorrectUserState ? <div className="extra section"> <Warning label="user-not-found" /> </div> : null}
+                <button type="submit" id="signin-button" className="button login-button" onClick={this.handleSubmit}>
                   <div>{TAPi18n.__('sign-in')}</div>
                 </button>
               </form>
