8d15dc70965e9940d16fe0ef8e3275c10c395b45
Santiago Siri <santisiri@gmail.com>
2017-03-06 17:19:37 -0500

    displays list of voters

--- imports/ui/templates/components/decision/contract/contract.js
+++ imports/ui/templates/components/decision/contract/contract.js
@@ -1,7 +1,9 @@
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
+import { Meteor } from 'meteor/meteor';
 
 import { splitRender } from '/imports/ui/modules/split';
+import { userVotesInContract } from '/imports/startup/both/modules/User';
 
 import './contract.html';
 import '../title/title.js';
@@ -51,5 +53,20 @@ Template.contract.helpers({
       return Session.get('contract').ballotEnabled;
     }
     return false;
+  },
+  voter() {
+    const ledger = Session.get('contract').wallet.ledger;
+    const voters = [];
+    let hasVote;
+    for (const index in ledger) {
+      hasVote = 0;
+      if (!voters.find(ledger[index].entityId)) {
+        hasVote = userVotesInContract(Meteor.users.findOne({ _id: ledger[index].entityId }).profile.wallet, Session.get('contract')._id);
+        if (hasVote > 0) {
+          voters.push(ledger[index].entityId);
+        }
+      }
+    }
+    return voters;
   },
 });
