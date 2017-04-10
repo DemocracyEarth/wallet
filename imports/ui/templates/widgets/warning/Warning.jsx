408085ff1fff59020218eccf6c5e043febe9a7ef
Santiago Siri <santisiri@gmail.com>
2017-04-10 17:06:44 -0400

    allow html rendering in Warning react component

--- imports/ui/templates/widgets/warning/Warning.jsx
+++ imports/ui/templates/widgets/warning/Warning.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOMServer from 'react-dom/server';
 import { behave } from '/imports/ui/modules/animation';
 import { TAPi18n } from 'meteor/tap:i18n';
 
@@ -10,7 +11,7 @@ export default class Warning extends Component {
   render() {
     return (
       <ul className="w-list-unstyled warning-list animate" ref={(parent) => { this.parentNode = parent; }}>
-        <li className="warning" >{TAPi18n.__(this.props.label)}</li>
+        <li className="warning" dangerouslySetInnerHTML={{ __html: TAPi18n.__(this.props.label) }} />
       </ul>
     );
   }
