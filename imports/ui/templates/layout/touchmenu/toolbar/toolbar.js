f3b48d4c28069893d6b8dd1b17869c4dd4ef2737
Santiago Siri <santisiri@gmail.com>
2017-05-17 14:06:20 -0400

    disables post button when exceedes max chars allowed

--- imports/ui/templates/layout/touchmenu/toolbar/toolbar.js
+++ imports/ui/templates/layout/touchmenu/toolbar/toolbar.js
@@ -1,11 +1,10 @@
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
-import { $ } from 'meteor/jquery';
 
 import './toolbar.html';
 
 function isDisabled() {
-  return (Session.get('missingTitle') || Session.get('mistypedTitle') || Session.get('duplicateURL'));
+  return (Session.get('missingTitle') || Session.get('mistypedTitle') || Session.get('duplicateURL') || (Session.get('availableChars') < 0));
 }
 
 Template.toolbar.onRendered(() => {
