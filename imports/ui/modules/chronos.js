be1a3494d23499b6ed40eb4cb947f41ef1d3c79e
Santiago Siri <santisiri@gmail.com>
2017-06-07 17:14:10 -0400

    polished time

--- imports/ui/modules/chronos.js
+++ imports/ui/modules/chronos.js
@@ -1,42 +1,44 @@
 import { TAPi18n } from 'meteor/tap:i18n';
 
-let timeAgo = (date) => {
-    var seconds = Math.floor((new Date() - date) / 1000);
-    return buildSentence(seconds, 'ago');
-}
-
-let timeLeft = (date) => {
-  var seconds = Math.floor((date - new Date()) / 1000);
-  if (seconds > 0) {
-    return buildSentence(seconds, 'left');
-  } else {
-    return false;
-  }
-}
-
-let buildSentence = (seconds, mode) => {
-  var interval = Math.floor(seconds / 31536000);
+const buildSentence = (seconds, mode) => {
+  let interval = Math.floor(seconds / 31536000);
   if (interval > 1) {
-    return interval + ' ' + TAPi18n.__('years-' + mode);
+    return `${interval} ${TAPi18n.__(`years-${mode}`)}`;
   }
   interval = Math.floor(seconds / 2592000);
   if (interval > 1) {
-      return interval + " " + TAPi18n.__('months-' + mode);
+    return `${interval} ${TAPi18n.__(`months-${mode}`)}`;
   }
   interval = Math.floor(seconds / 86400);
   if (interval > 1) {
-      return interval + " " + TAPi18n.__('days-' + mode);
+    return `${interval} ${TAPi18n.__(`days-${mode}`)}`;
   }
   interval = Math.floor(seconds / 3600);
   if (interval > 1) {
-      return interval + " " + TAPi18n.__('hours-' + mode);
+    return `${interval} ${TAPi18n.__(`hours-${mode}`)}`;
   }
   interval = Math.floor(seconds / 60);
   if (interval > 1) {
-      return interval + " " + TAPi18n.__('minutes-' + mode);
+    return `${interval} ${TAPi18n.__(`minutes-${mode}`)}`;
   }
-  return Math.floor(seconds) + " " + TAPi18n.__('seconds-' + mode);
-}
+  if (seconds === 0) {
+    return TAPi18n.__('now');
+  }
+  return `${Math.floor(seconds)} ${TAPi18n.__(`seconds-${mode}`)}`;
+};
+
+const timeAgo = (date) => {
+  const seconds = Math.floor((new Date() - date) / 1000);
+  return buildSentence(seconds, 'ago');
+};
+
+const timeLeft = (date) => {
+  const seconds = Math.floor((date - new Date()) / 1000);
+  if (seconds > 0) {
+    return buildSentence(seconds, 'left');
+  }
+  return false;
+};
 
 export const timeSince = timeAgo;
 export const countdown = timeLeft;
