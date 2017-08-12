bb346454b4ad40c6497d466e87af77eb5ed64c48
domi41 <dominique.merle.41@gmail.com>
2017-08-12 03:57:44 +0200

    Add a preliminary step definition for "I fill the ... with ...".

--- features/step_definitions/browser-steps.js
+++ features/step_definitions/browser-steps.js
@@ -1,6 +1,20 @@
 
-import { log } from './support/utils';
+import { log, fail, getServer } from './support/utils';
 import { visit, getBrowser } from './support/browser';
+
+const findOneDomElement  = (name) => {
+    let element = null;
+
+    // todo : if name has spaces in it, make it camelCase or snake_case or union-case
+
+    const elementsById = getBrowser().elements(`#${name}`).value;
+    if (elementsById.length > 0) {
+        if (elementsById.length > 1) { fail(`There is more than one DOM element with the id '${name}'.`) }
+        element = elementsById[0];
+    }
+
+    return element;
+};
 
 export default function () {
 
@@ -12,8 +26,21 @@ export default function () {
         widgets.fab.click();
     });
 
-    this.Then(/^I wait (?:for )?(\d+(?:\.\d*)?) ?s(?:econds?)?$/, (seconds) => {
-        getBrowser().pause(parseFloat(seconds) * 1000)
+    this.Then(/^I (?:wait|pause) (?:for )?(\d+(?:\.\d*)?) ?s(?:econds?)?$/, (seconds) => {
+        getBrowser().pause(parseFloat(seconds) * 1000);
+    });
+
+    this.When(/^I fill the (.+) with "(.*)"$/, (elementQuery, content) => {
+        let element = findOneDomElement(elementQuery);
+
+        if ( ! element) { fail(`Could not find any editable DOM element for query '${name}'.`); }
+
+        if (element.getAttribute("contenteditable")) {
+            element.click();
+            element.keys(content);
+        } else {  // todo : add support for input fields
+            fail(`DOM element found for query '${name}' is not a contenteditable.`);
+        }
     });
 
 };
