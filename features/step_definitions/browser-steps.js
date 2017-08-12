6a75eed6405aa5a68d91933ef125120e1e7d4e33
domi41 <dominique.merle.41@gmail.com>
2017-08-12 06:06:17 +0200

    Add a homemade `hasClass(element, classesAsString)` because webdriver's API does not seem to handle it.

--- features/step_definitions/browser-steps.js
+++ features/step_definitions/browser-steps.js
@@ -2,7 +2,7 @@
 import { log, fail, getServer, camelCase } from './support/utils';
 import { visit, getBrowser } from './support/browser';
 
-const findOneDomElement  = (name) => {
+const findOneDomElement = (name) => {
     name = camelCase(name);
 
     let element = null;
@@ -15,6 +15,20 @@ const findOneDomElement  = (name) => {
     return element;
 };
 
+
+/**
+ * Compensate for the absence of element.hasClass('...') in the webdriver API.
+ *
+ * @param element An element from the webdriver. http://webdriver.io/api.html
+ * @param classesAsString One class (eg: 'editable') or the intersection of multiple classes (eg: 'alert button').
+ */
+const hasClass = (element, classesAsString) => {
+    const unique = (e, i, self) => { return i == self.indexOf(e); };
+    const requiredClasses = classesAsString.split(' ').filter(unique);
+    const existingClasses = element.getAttribute('class').split(' ').filter(unique);
+
+    return existingClasses.concat(requiredClasses).filter(unique).length == existingClasses.length;
+};
 
 
 export default function () {
@@ -36,11 +50,14 @@ export default function () {
 
         if ( ! element) { fail(`Could not find any editable DOM element for query '${elementQuery}'.`); }
 
-        if (element.getAttribute("contenteditable")) {
+        if (element.getAttribute('contenteditable')) {
             element.click();
             element.keys(content);
+        } else if (hasClass(element, 'editable')) {
+          element.click();
+          element.keys(content);
         } else {  // todo : add support for input fields
-            fail(`DOM element found for query '${name}' is not a contenteditable.`);
+            fail(`DOM element found for query '${elementQuery}' seems not editable.`);
         }
     });
 
