24adb0ce052b71f754af3f454194aade5979dcf8
domi41 <dominique.merle.41@gmail.com>
2017-08-13 06:52:31 +0200

    Remove usage of the `browser.pause()` hack in the login process, shaving almost a whole second off the login step.

--- features/step_definitions/support/fixtures/user-fixtures.js
+++ features/step_definitions/support/fixtures/user-fixtures.js
@@ -1,48 +1,47 @@
-import { getBrowser, getServer } from '../utils';
-
+import {log, fail, getBrowser, getServer} from '../utils';
 
 
 fixtures.users = {
 
-    // create() {
-    //     server.execute(() => {
-    //         const email = 'test@example.com';
-    //         const password = 'jkjkjkjk';
-    //         let userId;
-    //         try {
-    //             const user = Meteor.users.findOne({ emails: { $elemMatch: { address: email } } });
-    //             userId = user._id;
-    //         } catch (e) {
-    //             userId = Accounts.createUser({
-    //                 username: 'test',
-    //                 email,
-    //                 password,
-    //                 profile: { firstName: 'Tim', lastName: 'Fletcher' },
-    //             });
-    //         }
-    //         // Always reset user password as a test may have changed it
-    //         Accounts.setPassword(userId, password, { logout: false });
-    //     });
-    // },
+  // create() {
+  //     server.execute(() => {
+  //         const email = 'test@example.com';
+  //         const password = 'jkjkjkjk';
+  //         let userId;
+  //         try {
+  //             const user = Meteor.users.findOne({ emails: { $elemMatch: { address: email } } });
+  //             userId = user._id;
+  //         } catch (e) {
+  //             userId = Accounts.createUser({
+  //                 username: 'test',
+  //                 email,
+  //                 password,
+  //                 profile: { firstName: 'Tim', lastName: 'Fletcher' },
+  //             });
+  //         }
+  //         // Always reset user password as a test may have changed it
+  //         Accounts.setPassword(userId, password, { logout: false });
+  //     });
+  // },
 
-    serverLogin(email, password) {
-        getServer().call('login', { user: { email: email }, password: password });
-    },
+  serverLogin(email, password) {
+    getServer().call('login', {user: {email: email}, password: password});
+  },
 
-    clientLogin(email, password) {
-        getBrowser().execute((email, password, done) => {
-            //console.log("I am printed in the browser's developer console.");
-            Meteor.loginWithPassword(email, password, done);
-            // note: async/wait does not work in here, no idea why
-        }, email, password);
+  clientLogin(email, password) {
+    getBrowser().timeoutsAsyncScript(2000);
+    const returned = getBrowser().executeAsync((email, password, done) => {
+      //console.log("I am printed in the browser's developer console.");
+      Meteor.loginWithPassword(email, password, (err) => { done(err); });
+    }, email, password);
 
-        // There has got to be a better way to wait for meteor login to complete.
-        // I tried async/wait, and flavors of the `done` callback. Nothing stuck.
-        getBrowser().pause(1000);
-    },
+    if (returned && returned.value) {
+      fail(`There was an error with the Meteor login : ${returned.value.message}.`);
+    }
+  },
 
-    login(email, password) {
-        this.serverLogin(email, password);
-        this.clientLogin(email, password);
-    },
+  login(email, password) {
+    this.serverLogin(email, password);
+    this.clientLogin(email, password);
+  },
 };
