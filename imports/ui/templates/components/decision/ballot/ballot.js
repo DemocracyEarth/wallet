17acbd055782f0cb35f29104bf051ae7d14b4ac4
Santiago Siri <santisiri@gmail.com>
2017-09-21 16:09:35 -0400

    ballot displays total number of voters

--- imports/ui/templates/components/decision/ballot/ballot.js
+++ imports/ui/templates/components/decision/ballot/ballot.js
@@ -3,6 +3,7 @@ import { Template } from 'meteor/templating';
 import { $ } from 'meteor/jquery';
 import { Session } from 'meteor/session';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { TAPi18n } from 'meteor/tap:i18n';
 
 import { removeFork, updateBallotRank, addChoiceToBallot, candidateBallot, getBallot } from '/imports/ui/modules/ballot';
 import { displayTimedWarning } from '/lib/utils';
@@ -267,6 +268,9 @@ Template.ballot.helpers({
   },
   candidateBallot() {
     return Template.instance().candidateBallot.get();
+  },
+  voters() {
+    return `0 ${TAPi18n.__('voters')}.`;
   },
 });
 
