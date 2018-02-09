f370652bae17f7bb178900bf8507f5f321bb5519
Santiago Siri <santisiri@gmail.com>
2018-02-08 19:39:16 -0500

    removed fast render due to long waiton times

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,10 +1,7 @@
-import { Meteor } from 'meteor/meteor';
 import { Router } from 'meteor/iron:router';
 import { Session } from 'meteor/session';
-import { TAPi18n } from 'meteor/tap:i18n';
-
-
 import { gui } from '/lib/const';
+
 import '/imports/ui/templates/layout/main.js';
 import '/imports/ui/templates/layout/url/home/home.js';
 import '/imports/ui/templates/layout/url/notFound/notFound.js';
@@ -13,8 +10,7 @@ import '/imports/ui/templates/components/identity/login/login.js';
 import '/imports/ui/templates/components/identity/card/card.js';
 import '/imports/ui/templates/components/decision/contract/contract.js';
 import '/imports/ui/templates/widgets/feed/feed.js';
-import { Contracts } from '../../api/contracts/Contracts';
-import { fn } from './functions';
+
 
 /*
 * router settings
@@ -32,7 +28,6 @@ Router.route('/', {
   name: 'home',
   template: 'home',
   loadingTemplate: 'load',
-  fastRender: true,
   onBeforeAction() {
     this.next();
   },
@@ -88,66 +83,6 @@ Router.route('/token/:hashtag', {
     return {
       options: { view: 'tag', sort: { createdAt: -1 }, limit: gui.ITEMS_PER_PAGE, skip: 0, tag: this.params.hashtag },
     };
-  },
-});
-
-/**
-* routing for feeds displaying contracts
-* NOTE: called when item clicked on sidebar menu
-**/
-Router.route('/:feed', {
-  name: 'homeFeed',
-  template: 'home',
-  waitOn() {
-    return [Meteor.subscribe('contracts', fn.buildQuery(this.params.query)), Meteor.subscribe('userData'), Meteor.subscribe('transactions')];
-  },
-  onBeforeAction() {
-    if (this.params.feed === 'feed') {
-      fn.clearSessionVars();
-      fn.configNavbar(fn.buildTitle(this.params.query), '/');
-      fn.setSessionVars(this.params);
-      Session.set('feed', Contracts.find(fn.buildQuery(this.params.query), { sort: { createdAt: -1 } }).fetch());
-      this.next();
-    } else {
-      this.render('notFound');
-    }
-  },
-});
-
-/**
-* @summary loads a contract meant for voting either to edit or vote.
-*/
-Router.route('/vote/:contract', {
-  name: 'voteContract',
-  template: 'contract',
-  waitOn() {
-    return [Meteor.subscribe('contracts', fn.buildQuery(this.params.query)), Meteor.subscribe('userData'), Meteor.subscribe('transactions')];
-  },
-  onBeforeAction() {
-    fn.clearSessionVars();
-    fn.configNavbar(fn.buildTitle(this.params.contract), '/vote');
-    fn.setSessionVars(this.params);
-    this.next();
-  },
-});
-
-/*
-* loads a contract meant for delegation of votes.
-****/
-Router.route('/delegation/:contract', {
-  name: 'delegationContract',
-  template: 'contract',
-  waitOn() {
-    if (this.params.contract === 'draft') {
-      return [Meteor.subscribe('contracts', { keyword: this.params.contract }), Meteor.subscribe('userData'), Meteor.subscribe('tags')];
-    }
-    return [Meteor.subscribe('contracts', { keyword: this.params.contract }), Meteor.subscribe('userData')];
-  },
-  onBeforeAction() {
-    fn.clearSessionVars();
-    fn.configNavbar(TAPi18n.__('navbar-delegation'), '/delegation');
-    fn.setSessionVars(this.params);
-    this.next();
   },
 });
 
