b8a825a5e675097406f06116592797efd5ceb6d4
Santiago Siri <santisiri@gmail.com>
2018-02-22 17:13:57 -0500

    proper ux for delegation status on cards

--- imports/ui/templates/widgets/transaction/transaction.js
+++ imports/ui/templates/widgets/transaction/transaction.js
@@ -1,9 +1,13 @@
 import { Template } from 'meteor/templating';
 import { TAPi18n } from 'meteor/tap:i18n';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import { getVotes } from '/imports/api/transactions/transaction';
 
 import './transaction.html';
 
 Template.transaction.onCreated(function () {
+  Template.instance().totalVotes = new ReactiveVar(0);
 });
 
 Template.transaction.helpers({
@@ -30,11 +34,23 @@ Template.transaction.helpers({
     };
   },
   value() {
-    // TODO: this data should be from ledger
-    return this.contract.wallet.available;
+    Template.instance().totalVotes.set(getVotes(this.contract._id, this.senderId));
+    const votes = Template.instance().totalVotes.get();
+    if (votes === 1) {
+      return `${votes} ${TAPi18n.__('vote')}`;
+    } else if (votes > 0) {
+      return `${votes} ${TAPi18n.__('votes')}`;
+    }
+    return TAPi18n.__('no-delegated-votes');
   },
   source() {
     return TAPi18n.__('delegated-votes');
+  },
+  voteStyle() {
+    if (Template.instance().totalVotes.get() > 0) {
+      return 'stage stage-finish-approved';
+    }
+    return 'stage stage-live';
   },
   emptyVotes() {
     // TODO: this data should be from ledger
