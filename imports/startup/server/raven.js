5cfe8e91667ed41585acd52840b9eb3e26999e86
Santiago Siri <santisiri@gmail.com>
2018-05-17 11:22:36 -0400

    fixed editor to support multiple tags

--- imports/startup/server/raven.js
+++ imports/startup/server/raven.js
@@ -2,13 +2,11 @@ import RavenLogger from 'meteor/flowkey:raven';
 import { Meteor } from 'meteor/meteor';
 
 const ravenOptions = {};
-console.log('------------ SDJKFDKJHFKSJD ')
+
 const ravenLogger = new RavenLogger({
   privateDSN: Meteor.settings.private.sentryPrivateDSN,
   shouldCatchConsoleError: true, // default true
   trackUser: true, // default false
 }, ravenOptions);
-
-console.log(ravenLogger);
 
 export default ravenLogger;
