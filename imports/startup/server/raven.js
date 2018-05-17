375ad194a0243197050ff6c17afc921ed0879033
Santiago Siri <santisiri@gmail.com>
2018-05-17 11:08:18 -0400

    server side config of raven fixed

--- imports/startup/server/raven.js
+++ imports/startup/server/raven.js
@@ -2,11 +2,13 @@ import RavenLogger from 'meteor/flowkey:raven';
 import { Meteor } from 'meteor/meteor';
 
 const ravenOptions = {};
-
+console.log('------------ SDJKFDKJHFKSJD ')
 const ravenLogger = new RavenLogger({
-  privateDSN: Meteor.settings.sentryPrivateDSN,
+  privateDSN: Meteor.settings.private.sentryPrivateDSN,
   shouldCatchConsoleError: true, // default true
   trackUser: true, // default false
 }, ravenOptions);
+
+console.log(ravenLogger);
 
 export default ravenLogger;
