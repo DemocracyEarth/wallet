db2e853a1c9bad624f904dc854eb97c30a8ed587
Santiago Siri <santisiri@gmail.com>
2018-07-19 16:54:08 +0200

    first hook from lists to ui

--- imports/ui/templates/components/decision/constituency/constituency.js
+++ imports/ui/templates/components/decision/constituency/constituency.js
@@ -6,11 +6,47 @@ import { Session } from 'meteor/session';
 import { animatePopup } from '/imports/ui/modules/popup';
 import '/imports/ui/templates/components/decision/constituency/constituency.html';
 
+Template.constituency.onCreated(() => {
+  Session.set('showNations', false);
+  Session.set('showTokens', false);
+});
+
+Template.constituency.helpers({
+  showNations() {
+    return Session.get('showNations');
+  },
+  showTokens() {
+    return Session.get('showTokens');
+  },
+  country() {
+    if (Session.get('newCountry') !== undefined) {
+      return Session.get('newCountry').name;
+    }
+    return undefined;
+  },
+  token() {
+    if (Session.get('newToken') !== undefined) {
+      return Session.get('newToken').name;
+    }
+    return undefined;
+  },
+});
+
 Template.constituency.events({
   'click #cancel-constituency'() {
     animatePopup();
   },
   'click #execute-constituency'() {
 
+  },
+  'focus .country-search'() {
+    Session.set('showNations', true);
+  },
+  'focus .login-input-split-right'() {
+    Session.set('showNations', false);
+    Session.set('showTokens', false);
+  },
+  'focus .token-search'() {
+    Session.set('showTokens', true);
   },
 });
