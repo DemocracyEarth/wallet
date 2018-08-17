bfc47efa58513f95fc057ff77a05eed59a166867
Santiago Siri <santisiri@gmail.com>
2018-08-17 17:19:23 -0400

    displays logger again

--- imports/ui/templates/components/collective/collective.js
+++ imports/ui/templates/components/collective/collective.js
@@ -1,8 +1,36 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Router } from 'meteor/iron:router';
+import { Session } from 'meteor/session';
+import { $ } from 'meteor/jquery';
 
-import './collective.html';
+import { displayPopup, animatePopup } from '/imports/ui/modules/popup';
+
+import '/imports/ui/templates/components/collective/collective.html';
+
+function promptLogin(logged, event) {
+  if (logged) {
+    Session.set('userLoginVisible', true);
+    displayPopup($('#collective-login')[0], 'login', Meteor.userId(), event.type, 'user-login');
+  } else {
+    Session.set('userLoginVisible', false);
+    animatePopup(false, 'user-login');
+  }
+}
+
+Template.collective.onRendered(() => {
+  Session.set('userLoginVisible', false);
+  if (!Session.get('checkInitialSetup') && Meteor.userId() === null) {
+    promptLogin(true, 'click');
+    Session.set('checkInitialSetup', true);
+  }
+
+  window.addEventListener('click', function (e) {
+    if (!document.getElementById('card-user-login').contains(e.target)) {
+      promptLogin((!Session.get('user-login') || !Session.get('user-login').visible), event);
+    }
+  });
+});
 
 Template.collective.helpers({
   title() {
@@ -20,10 +48,20 @@ Template.collective.helpers({
   hasLogo() {
     return (Meteor.settings.public.Collective.profile.logo !== undefined);
   },
+  toggle() {
+    if (Session.get('userLoginVisible')) {
+      return 'collective-selected';
+    }
+    return '';
+  },
 });
 
 Template.collective.events({
   'click #collective-home'() {
     Router.go('/');
+  },
+  'click #collective-login'() {
+    event.stopPropagation();
+    promptLogin((!Session.get('user-login') || !Session.get('user-login').visible), event);
   },
 });
