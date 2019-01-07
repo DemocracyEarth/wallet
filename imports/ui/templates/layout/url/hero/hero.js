0092d36d6a6c34646640e3996e46957d12dda9f0
Santiago Siri <santisiri@gmail.com>
2019-01-07 18:17:30 -0500

    night

--- imports/ui/templates/layout/url/hero/hero.js
+++ imports/ui/templates/layout/url/hero/hero.js
@@ -6,3 +6,16 @@ import { Meteor } from 'meteor/meteor';
 import { TAPi18n } from 'meteor/tap:i18n';
 
 import '/imports/ui/templates/layout/url/hero/hero.html';
+import '/imports/ui/templates/components/collective/collective.js';
+
+Template.hero.helpers({
+  title() {
+    return TAPi18n.__('landing-title');
+  },
+  about() {
+    return TAPi18n.__('landing-tagline');
+  },
+  logo() {
+    return `${Router.path('home')}images/democracy-earth-white.png`;
+  },
+});
