f3c8f02aaf3721c63911e0bbe77e67cd0fdf8990
Santiago Siri <santisiri@gmail.com>
2019-01-24 18:21:57 -0500

    raw application of a css to app

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,6 +1,6 @@
 import '/imports/ui/css/lib/normalize.css';
 import '/imports/ui/css/lib/webflow.css';
-import '/imports/ui/css/earth.css';
+import '/imports/ui/css/sovereign.css';
 import '/imports/ui/css/extra.css';
 import '/imports/ui/css/medium-editor.css';
 import '/imports/ui/css/medium-editor-insert-plugin.css';
