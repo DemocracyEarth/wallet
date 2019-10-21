534edf67d60db94a8b8a906b072e1c4a21a7668a
Santiago Siri <santisiri@gmail.com>
2019-10-21 16:31:45 +0200

    styling for blockies

--- imports/ui/templates/components/identity/avatar/avatar.js
+++ imports/ui/templates/components/identity/avatar/avatar.js
@@ -452,22 +452,6 @@ Template.avatar.events({
   },
 });
 
-Template.blockie.onRendered(function () {
-  const icon = blockies.create({ // All options are optional
-    seed: this.address, // seed used to generate icon data, default: random
-    color: '#dfe', // to manually specify the icon color, default: random
-    bgcolor: '#aaa', // choose a different background color, default: random
-    size: 15, // width/height of the icon in blocks, default: 8
-    scale: 3, // width/height of each block in pixels, default: 4
-    spotcolor: '#000' // each pixel has a 13% chance of being of a third color, 
-    // default: random. Set to -1 to disable it. These "spots" create structures
-    // that look like eyes, mouths and noses. 
-  });
-  console.log(this.data.address);
-  console.log(this.data.elementId);
-  console.log(icon);
-});
-
 export const getFlag = getNation;
 export const getUser = _getUser;
 export const shortenCryptoName = _shortenCryptoName;
