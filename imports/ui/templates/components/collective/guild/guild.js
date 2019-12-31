06079032de27ff6d1d25e0a605689668553a5441
santisiri <santisiri@gmail.com>
2019-12-31 20:56:50 +0100

    basic guild ui

--- imports/ui/templates/components/collective/guild/guild.js
+++ imports/ui/templates/components/collective/guild/guild.js
@@ -1,1 +1,32 @@
-import '/imports/ui/templates/components/decision/guild/guild.html';
+import { Template } from 'meteor/templating';
+
+import '/imports/ui/templates/components/collective/guild/guild.html';
+import '/imports/ui/templates/components/decision/balance/balance.js';
+
+const standardBalance = {
+  token: 'MOLOCH',
+  balance: 0,
+  available: 0,
+  placed: 0,
+  isTransaction: true,
+  isRevoke: false,
+  date: new Date(),
+  disableBar: true,
+  disableStake: true,
+  value: 100,
+};
+
+Template.guild.helpers({
+  totalShares() {
+    return standardBalance;
+  },
+  totalAssets() {
+    return standardBalance;
+  },
+  shareValue() {
+    return standardBalance;
+  },
+  totalValue() {
+    return standardBalance;
+  },
+});
