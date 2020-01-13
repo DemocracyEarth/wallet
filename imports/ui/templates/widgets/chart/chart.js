4ab197d8642602b8c47e29f1ce8a9e48c863bfe9
Santiago Siri <santisiri@gmail.com>
2020-01-13 15:06:03 +0100

    charts styling minimalism

--- imports/ui/templates/widgets/chart/chart.js
+++ imports/ui/templates/widgets/chart/chart.js
@@ -9,34 +9,50 @@ const _setupChart = (collectiveId) => {
   const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Total Scorer',
+        data: [5904, 5993, 6040, 6493, 6599, 7005, 3040, 5030, 4500, 6000, 5960, 7690],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
+          '#00c0912e',
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
+          '#00c091',
         ],
-        borderWidth: 1,
+        borderWidth: 0,
       }],
     },
     options: {
+      responsive: false,
+      maintainAspectRatio: true,
+      legend: {
+        display: false,
+      },
       scales: {
         yAxes: [{
+          display: false,
+          gridLines: {
+            display: false,
+          },
+          stacked: true,
           ticks: {
             beginAtZero: true,
+            maxTicksLimit: 6,
+            suggestedMin: 5904,
+            suggestedMax: 7690,
+          },
+        }],
+        xAxes: [{
+          display: false,
+          gridLines: {
+            display: false,
+          },
+          stacked: true,
+          ticks: {
+            beginAtZero: true,
+            maxTicksLimit: 6,
+            suggestedMin: 5904,
+            suggestedMax: 7690,
           },
         }],
       },
