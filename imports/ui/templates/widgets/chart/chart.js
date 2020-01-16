4f37db3fb9a626166f4ecd128c517fe84b778d1c
Santiago Siri <santisiri@gmail.com>
2020-01-16 17:18:57 +0100

    first decent render

--- imports/ui/templates/widgets/chart/chart.js
+++ imports/ui/templates/widgets/chart/chart.js
@@ -9,30 +9,15 @@ const defaultChart = {
   type: 'line',
   data: {
     datasets: [{
-      data: [
-        {
-          t: new Date("2015-3-15 13:3"),
-          y: 12,
-        },
-        {
-          t: new Date("2015-3-25 13:2"),
-          y: 21,
-        },
-        {
-          t: new Date("2015-4-25 14:12"),
-          y: 32,
-        },
-      ],
-      backgroundColor: [
-        '#00c0912e',
-      ],
-      borderColor: [
-        '#00c091',
-      ],
-      borderWidth: 0,
+      data: [],
     }],
   },
   options: {
+    elements: {
+      point: {
+        radius: 0,
+      },
+    },
     responsive: true,
     maintainAspectRatio: true,
     legend: {
@@ -71,19 +56,11 @@ const _setupChart = (collectiveId, guildLabel) => {
   const ctx = document.getElementById(`collectiveChart-${collectiveId}`);
   const collective = Collectives.findOne({ _id: collectiveId });
 
-  console.log(JSON.stringify(defaultChart.data.datasets));
-
-  console.log(JSON.stringify(collective.profile.chart));
-
   const finalChart = defaultChart;
   finalChart.data.datasets = _.findWhere(collective.profile.chart, { guildLabel }).dataset;
   finalChart.data.datasets[0].backgroundColor = ['#00c0912e'];
   finalChart.data.datasets[0].borderColor = ['#00c091'];
   finalChart.data.datasets[0].borderWidth = 0;
-
-  console.log(JSON.stringify(finalChart.data.datasets));
-  console.log(JSON.stringify(collective.profile.guild));
-
 
   return new Chart(ctx, finalChart);
 };
