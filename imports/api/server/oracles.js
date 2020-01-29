1ef4b520155a65073f1e4dcc8cff91425fb3b6be
Santiago Siri <santisiri@gmail.com>
2020-01-29 19:45:25 +0100

    first gini calculation

--- imports/api/server/oracles.js
+++ imports/api/server/oracles.js
@@ -1,10 +1,27 @@
+import { Meteor } from 'meteor/meteor';
 import { Collectives } from '/imports/api/collectives/Collectives';
 
 import { log } from '/lib/const';
 
+const gini = require('gini');
+
 const _calculateGini = (collective) => {
   log(`{ oracle: 'calculateGini', collective: '${collective._id}' }`);
 
+  if (!collective.profile.replica) {
+    const members = Meteor.users.find({ 'profile.collectives': [collective._id] }).fetch();
+    const set = [];
+    for (const individual of members) {
+      for (const value of individual.profile.wallet.reserves) {
+        if (value.token === 'MOLOCH' && value.balance > 0) {
+          set.push(value.balance);
+        }
+      }
+    }
+
+    console.log('--------------------------');
+    console.log(gini.unordered(set));
+  }
 };
 
 const _calculateRanking = () => {
@@ -14,13 +31,10 @@ const _calculateRanking = () => {
 const _setReplicaScore = (user) => {
   console.log('--------------------------');
   log(`{ oracle: 'setReplicaScore', user: '${user._id}' }`);
-  console.log(user);
 
   let collective;
   for (const collectiveId of user.profile.collectives) {
-    console.log(user.profile.collectives);
     collective = Collectives.findOne({ _id: collectiveId });
-    console.log(collective);
     if (collective) {
       _calculateGini(collective);
     }
