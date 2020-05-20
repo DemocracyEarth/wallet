33b787b63de308db792a87fe59f65bd96a59d500
Santiago Siri <santisiri@gmail.com>
2020-05-20 10:42:21 +0200

    block animation in the dapp

--- imports/ui/templates/layout/url/synchronizer/synchronizer.js
+++ imports/ui/templates/layout/url/synchronizer/synchronizer.js
@@ -1,1 +1,17 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { templetize, getImage } from '/imports/ui/templates/layout/templater';
+
 import '/imports/ui/templates/layout/url/synchronizer/synchronizer.html';
+
+Template.synchronizer.onCreated(function () {
+  Template.instance().imageTemplate = new ReactiveVar();
+  const instance = Template.instance();
+  templetize(instance);
+});
+
+Template.synchronizer.helpers({
+  getImage(pic) {
+    return getImage(Template.instance().imageTemplate.get(), pic);
+  },
+});
