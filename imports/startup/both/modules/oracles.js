4a5cc4314b75954f7d8a121411548ecb198fc0f7
Santiago Siri <santisiri@gmail.com>
2020-06-25 18:27:48 +0200

    oracle ranks daos by economic weight

--- imports/startup/both/modules/oracles.js
+++ imports/startup/both/modules/oracles.js
@@ -2,8 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Collectives } from '/imports/api/collectives/Collectives';
 
 import { getBlockHeight } from '/lib/web3';
-import { log } from '/lib/const';
-import { defaults } from '../../../../lib/const';
+import { log, defaults } from '/lib/const';
 
 const giniCalculator = require('gini');
 
@@ -13,7 +12,7 @@ const giniCalculator = require('gini');
 * @return {number} with gini value
 */
 const _calculateGini = (collective) => {
-  log(`[oracle] Calculating Gini coefficient for collective: '${collective._id}'`);
+  log(`[oracle] Calculating Gini coefficient for collective: '${collective.uri}'`);
 
   const members = Meteor.users.find({ 'profile.collectives': collective._id }).fetch();
   const set = [];
@@ -36,24 +35,37 @@ const _calculateGini = (collective) => {
 /**
 * @summary caclulcates the ranking of a collective based on the votes of participants
 */
-const _calculateRanking = () => {
-  return 0;
+const _calculateRankings = () => {
+  const values = [];
+  const collectives = _.sortBy(Collectives.find().fetch(), (item) => { values.push(_.findWhere(item.profile.guild, { name: 'guild-total-value' }).value.toNumber()); return values.slice(-1); });
+  const valueList = _.sortBy(_.uniq(values), (num) => { return num; });
+  let daoValue;
+  let position;
+  for (const dao of collectives) {
+    daoValue = _.findWhere(dao.profile.guild, { name: 'guild-total-value' }).value.toNumber();
+    position = _.indexOf(valueList, daoValue).toNumber();
+    dao.profile.replica.ranking = parseFloat(position / (valueList.length - 1), 10);
+
+    log(`[oracle] The DAO ${dao.name} has a ranking of ${dao.profile.replica.ranking}`);
+    Collectives.update({ _id: dao._id }, { $set: { 'profile.replica.ranking': dao.profile.replica.ranking } });
+  }
 };
 
 /**
 * @summary persists the score of a replica to a given collective
 * @param {string} collectiveId to calculate different replica attributes
+* @param {number} height of current chain
 * @return {object} with replica values
 */
-const _setCollectiveReplicaScore = (collectiveId) => {
-  log(`[oracle] Setting replica score for collective: '${collectiveId}'`);
+const _setCollectiveReplicaScore = async (collectiveId, height) => {
   const collective = Collectives.findOne({ _id: collectiveId });
+  log(`[oracle] Setting replica score for dao: '${collective.uri}'`);
   let replica;
   if (collective) {
     const gini = _calculateGini(collective);
-    const ranking = _calculateRanking(collective);
+    const ranking = collective.profile.replica.ranking;
     const score = parseFloat(((1 - gini) + ranking) / 2, 10);
-    const lastSyncedBlock = collective.profile.lastSyncedBlock ? collective.profile.lastSyncedBlock : 0;
+    const lastSyncedBlock = height; // await getBlockHeight(); // 0 // collective.profile.lastSyncedBlock ? collective.profile.lastSyncedBlock : 0;
 
     replica = {
       lastSyncedBlock,
@@ -63,7 +75,7 @@ const _setCollectiveReplicaScore = (collectiveId) => {
     };
 
     if (!collective.profile.replica || (collective.profile.replica && collective.profile.replica.lastSyncedBlock < lastSyncedBlock)) {
-      log(`[oracle] Updating collective ${collectiveId} with replica: ${JSON.stringify(replica)}`);
+      log(`[oracle] Updating collective ${collective.uri} with replica: ${JSON.stringify(replica)}`);
       Collectives.update({ _id: collectiveId }, { $set: { 'profile.replica': replica } });
     }
   }
@@ -73,15 +85,16 @@ const _setCollectiveReplicaScore = (collectiveId) => {
 /**
 * @summary calculates the score of a user and every related collective
 * @param {object} user that needs update on replica score
+* @param {number} height of current chain
 * @return {object} with replica values
 */
-const _setReplicaScore = (user) => {
+const _setReplicaScore = (user, height) => {
   const collectiveReplicas = [];
   if (user.profile.collectives && user.profile.collectives.length > 0) {
-    log(`[oracle] Setting replica score for user: '${user._id}'`);
+    log(`[oracle] Setting replica score for user: '${user.username}'`);
 
     for (const collectiveId of user.profile.collectives) {
-      collectiveReplicas.push(_setCollectiveReplicaScore(collectiveId));
+      collectiveReplicas.push(_setCollectiveReplicaScore(collectiveId, height));
     }
 
     if (collectiveReplicas.length > 0) {
@@ -104,13 +117,17 @@ const _setReplicaScore = (user) => {
 /**
 * @summary general function to call oracles
 */
-const _oracles = () => {
-  const blockHeight = getBlockHeight();
-  const pendingReplicas = Meteor.users.find({ $or: [{ 'profile.replica': { $exists: false } }, { 'profile.replica.lastSyncedBlock': { $lt: parseInt(blockHeight - defaults.ORACLE_BLOCKTIME, 10) } }] }).count();
+const _oracles = async () => {
+  const blockHeight = await getBlockHeight();
+  const pendingReplicas = Meteor.users.find({ $or: [{ 'profile.replica': { $exists: false } }, { 'profile.replica.lastSyncedBlock': { $lt: parseInt(blockHeight - defaults.ORACLE_BLOCKTIME, 10) } }] }).fetch();
+
   log(`[oracle] Refreshing replica scores for ${pendingReplicas.length} users...`);
 
-  for (const user of pendingReplicas) {
-    _setReplicaScore(user);
+  if (pendingReplicas.length > 0) {
+    _calculateRankings();
+    for (const user of pendingReplicas) {
+      _setReplicaScore(user, blockHeight);
+    }
   }
 };
 
