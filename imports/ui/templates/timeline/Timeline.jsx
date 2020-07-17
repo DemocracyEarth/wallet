ec0fcd6d4db49574563da77892db3c4a2732be17
Santiago Siri <santisiri@gmail.com>
2020-07-17 18:28:40 +0200

    refactor to lightier and more modular components

--- imports/ui/templates/timeline/Timeline.jsx
+++ imports/ui/templates/timeline/Timeline.jsx
@@ -2,13 +2,27 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ApolloClient, { gql, InMemoryCache } from 'apollo-boost';
 import { ApolloProvider, Query } from 'react-apollo';
+import { TAPi18n } from 'meteor/tap:i18n';
 
+import Account from '/imports/ui/templates/timeline/account/Account.jsx';
 import Post from '/imports/ui/templates/timeline/post/Post.jsx';
+import Stamp from '/imports/ui/templates/timeline/stamp/Stamp.jsx';
+import Parameter from '/imports/ui/templates/timeline/parameter/Parameter.jsx';
+import Token from '/imports/ui/templates/timeline/token/Token.jsx';
+import Countdown from '/imports/ui/templates/timeline/countdown/Countdown.jsx';
+import Poll from '/imports/ui/templates/timeline/poll/Poll.jsx';
+import Choice from '/imports/ui/templates/timeline/choice/Choice.jsx';
+import Period from '/imports/ui/templates/timeline/period/Period.jsx';
+import Contract from '/imports/ui/templates/timeline/contract/Contract.jsx';
 
 const client = new ApolloClient({
   uri: Meteor.settings.public.graph.timeline,
   cache: new InMemoryCache(),
 });
+
+const _getPercentage = (percentageAmount, remainder) => {
+  return parseFloat((percentageAmount * 100) / (percentageAmount + remainder), 10);
+};
 
 const ProposalQuery = () => {
   return (
@@ -49,35 +63,58 @@ const ProposalQuery = () => {
 
         console.log(data.proposals);
 
-        let accountAddress;
-        if (Meteor.user()) {
-          accountAddress = Meteor.user().username;
-        }
+        const accountAddress = Meteor.user().username || null;
+        const daoName = 'MolochDAO';
+        const publicAddress = '0x1fd169a4f5c59acf79d0fd5d91d1201ef1bce9f1';
 
         return data.proposals.map((proposal) => {
+          const totalVoters = parseInt(proposal.yesVotes.toNumber() + proposal.noVotes.toNumber(), 10).toString();
+          const yesPercentage = _getPercentage(proposal.yesShares.toNumber(), proposal.noShares.toNumber()).toString();
+          const noPercentage = _getPercentage(proposal.noShares.toNumber(), proposal.yesShares.toNumber()).toString();
+
           return (
             <Post
-              key={proposal.id}
-              id={`molochdao-${proposal.proposalIndex}`}
-              daoName={'MolochDAO'}
-              accountAddress={accountAddress}
-              publicAddress={'0x1fd169a4f5c59acf79d0fd5d91d1201ef1bce9f1'}
-              description={proposal.details}
-              memberAddress={proposal.memberAddress}
-              applicantAddress={proposal.applicant.applicantAddress}
-              timestamp={proposal.timestamp}
-              sharesRequested={proposal.sharesRequested}
-              tokenTribute={proposal.tokenTribute}
-              yesVotes={proposal.yesVotes}
-              noVotes={proposal.noVotes}
-              votingPeriodBegins={proposal.votingPeriodBegins}
-              votingPeriodEnds={proposal.votingPeriodEnds}
-              gracePeriodEnds={proposal.gracePeriodEnds}
-              yesShares={proposal.yesShares}
-              noShares={proposal.noShares}
-              proposalIndex={proposal.proposalIndex}
-              status={proposal.status}
-            />
+              key={proposal.id} accountAddress={accountAddress}
+              description={proposal.details} memberAddress={proposal.memberAddress}
+            >
+              <Contract>
+                <Parameter label={TAPi18n.__('moloch-applicant')}>
+                  <Account publicAddress={proposal.applicant.applicantAddress} width="24px" height="24px" />
+                </Parameter>
+                <Parameter label={TAPi18n.__('moloch-request')}>
+                  <Token quantity={proposal.sharesRequested.toString()} symbol="SHARES" />
+                </Parameter>
+                <Parameter label={TAPi18n.__('moloch-tribute')}>
+                  <Token quantity={proposal.tokenTribute.toString()} symbol="WETH" />
+                </Parameter>
+              </Contract>
+              <Stamp timestamp={proposal.timestamp} />
+              <Countdown
+                votingPeriodBegins={proposal.votingPeriodBegins} votingPeriodEnds={proposal.votingPeriodEnds} 
+                gracePeriodEnds={proposal.gracePeriodEnds} totalVoters={totalVoters}
+              />
+              <Poll>
+                <Choice
+                  accountAddress={accountAddress} daoName={daoName} publicAddress={publicAddress}
+                  proposalIndex={proposal.proposalIndex} label={TAPi18n.__('yes')} percentage={yesPercentage}
+                  voteValue={1} votingPeriodEnds={proposal.votingPeriodEnds} votingPeriodBegins={proposal.votingPeriodBegins}
+                >
+                  <Token quantity={proposal.yesVotes} symbol="SHARES" />
+                </Choice>
+                <Choice
+                  accountAddress={accountAddress} daoName={daoName} publicAddress={publicAddress}
+                  proposalIndex={proposal.proposalIndex} label={TAPi18n.__('no')} percentage={noPercentage}
+                  barStyle="poll-score-bar-fill-negative" voteValue={2} votingPeriodEnds={proposal.votingPeriodEnds}
+                  votingPeriodBegins={proposal.votingPeriodBegins}
+                >
+                  <Token quantity={proposal.noVotes} symbol="SHARES" />
+                </Choice>
+              </Poll>
+              <Period
+                status={proposal.status} votingPeriodBegins={proposal.votingPeriodBegins}
+                votingPeriodEnds={proposal.votingPeriodEnds} gracePeriodEnds={proposal.gracePeriodEnds}
+              />
+            </Post>
           );
         });
       }}
--- imports/ui/templates/timeline/post/Post.jsx
+++ imports/ui/templates/timeline/post/Post.jsx
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import parser from 'html-react-parser';
-import { TAPi18n } from 'meteor/tap:i18n';
 
 import { wrapURLs } from '/lib/utils';
 
 import Account from '/imports/ui/templates/timeline/account/Account.jsx';
-import Stamp from '/imports/ui/templates/timeline/stamp/Stamp.jsx';
-import Parameter from '/imports/ui/templates/timeline/parameter/Parameter.jsx';
-import Token from '/imports/ui/templates/timeline/token/Token.jsx';
-import Countdown from '/imports/ui/templates/timeline/countdown/Countdown.jsx';
-import Poll from '/imports/ui/templates/timeline/poll/Poll.jsx';
-import Choice from '/imports/ui/templates/timeline/choice/Choice.jsx';
-import Period from '/imports/ui/templates/timeline/period/Period.jsx';
-import Contract from '/imports/ui/templates/timeline/contract/Contract.jsx';
 
 /**
 * @summary quick function to determine if a string is a JSON
@@ -28,23 +19,13 @@ const _isJSON = (str) => {
   return true;
 };
 
-
-const _getPercentage = (percentageAmount, remainder) => {
-  return parseFloat((percentageAmount * 100) / (percentageAmount + remainder), 10);
-};
-
 /**
 * @summary renders a post in the timeline
 */
 export default class Post extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      yesPercentage: _getPercentage(this.props.yesShares.toNumber(), this.props.noShares.toNumber()).toString(),
-      noPercentage: _getPercentage(this.props.noShares.toNumber(), this.props.yesShares.toNumber()).toString(),
-    };
-    this.state = Object.assign(this.getDescription(), this.state);
+    this.state = this.getDescription();
   }
 
   getDescription() {
@@ -68,21 +49,9 @@ export default class Post extends Component {
     return content;
   }
 
-  getPeriodLabel() {
-    return this.props.yesVotes;
-  }
-
-  getPeriodStatus() {
-    return this.props.yesVotes;
-  }
-
-  totalVoters() {
-    return parseInt(this.props.yesVotes.toNumber() + this.props.noVotes.toNumber(), 10).toString();
-  }
-
   render() {
     return (
-      <div id={this.props.id} className="vote vote-search vote-feed nondraggable vote-poll" href={`/dao/${this.props.daoName}/proposal/${this.props.proposalIndex}`}>
+      <div className="vote vote-search vote-feed nondraggable vote-poll" href={`/dao/${this.props.daoName}/proposal/${this.props.proposalIndex}`}>
         <div className="checkbox checkbox-custom">
           <div className="meta meta-search meta-bar">
             <Account id={`avatar-${this.props.memberAddress}`} publicAddress={this.props.memberAddress} width="24px" height="24px" />
@@ -112,28 +81,7 @@ export default class Post extends Component {
               </div>
             </div>
           </div>
-          <Contract>
-            <Parameter label={TAPi18n.__('moloch-applicant')}>
-              <Account id={`avatar-${this.props.applicantAddress}`} publicAddress={this.props.applicantAddress} width="24px" height="24px" />
-            </Parameter>
-            <Parameter label={TAPi18n.__('moloch-request')}>
-              <Token quantity={this.props.sharesRequested.toString()} symbol="SHARES" />
-            </Parameter>
-            <Parameter label={TAPi18n.__('moloch-tribute')}>
-              <Token quantity={this.props.tokenTribute.toString()} symbol="WETH" />
-            </Parameter>
-          </Contract>
-          <Stamp timestamp={this.props.timestamp} />
-          <Countdown votingPeriodBegins={this.props.votingPeriodBegins} votingPeriodEnds={this.props.votingPeriodEnds} gracePeriodEnds={this.props.gracePeriodEnds} totalVoters={this.totalVoters()} />
-          <Poll>
-            <Choice accountAddress={this.props.accountAddress} daoName={this.props.daoName} publicAddress={this.props.publicAddress} proposalIndex={this.props.proposalIndex} label={TAPi18n.__('yes')} percentage={this.state.yesPercentage} voteValue={1} votingPeriodEnds={this.props.votingPeriodEnds} votingPeriodBegins={this.props.votingPeriodBegins}>
-              <Token quantity={this.props.yesVotes} symbol="SHARES" />
-            </Choice>
-            <Choice accountAddress={this.props.accountAddress} daoName={this.props.daoName} publicAddress={this.props.publicAddress} proposalIndex={this.props.proposalIndex} label={TAPi18n.__('no')} percentage={this.state.noPercentage} barStyle="poll-score-bar-fill-negative" voteValue={2} votingPeriodEnds={this.props.votingPeriodEnds} votingPeriodBegins={this.props.votingPeriodBegins}>
-              <Token quantity={this.props.noVotes} symbol="SHARES" />
-            </Choice>
-          </Poll>
-          <Period status={this.props.status} votingPeriodBegins={this.props.votingPeriodBegins} votingPeriodEnds={this.props.votingPeriodEnds} gracePeriodEnds={this.props.gracePeriodEnds} />
+          {this.props.children}
         </div>
       </div>
     );
@@ -141,23 +89,12 @@ export default class Post extends Component {
 }
 
 Post.propTypes = {
-  id: PropTypes.string,
-  publicAddress: PropTypes.string,
-  accountAddress: PropTypes.string,
   description: PropTypes.string,
   proposalIndex: PropTypes.string,
   daoName: PropTypes.string,
   memberAddress: PropTypes.string,
-  applicantAddress: PropTypes.string,
-  timestamp: PropTypes.string,
-  sharesRequested: PropTypes.string,
-  tokenTribute: PropTypes.string,
-  votingPeriodBegins: PropTypes.string,
-  votingPeriodEnds: PropTypes.string,
-  gracePeriodEnds: PropTypes.string,
-  yesVotes: PropTypes.string,
-  noVotes: PropTypes.string,
-  yesShares: PropTypes.string,
-  noShares: PropTypes.string,
-  status: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
 };
