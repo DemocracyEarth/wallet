7d7099b542add3a91fd2330efb0805820ffb25c6
santisiri <santisiri@gmail.com>
2020-07-21 19:29:53 +0200

    ens domains

--- imports/ui/components/Account/Account.jsx
+++ imports/ui/components/Account/Account.jsx
@@ -1,44 +1,102 @@
-import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { TAPi18n } from 'meteor/tap:i18n';
+
+import ApolloClient, { gql, InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { useQuery } from '@apollo/react-hooks';
 
 import { shortenCryptoName } from '/imports/startup/both/modules/metamask';
 
+
 const makeBlockie = require('ethereum-blockies-base64');
+
+const client = new ApolloClient({
+  uri: Meteor.settings.public.graph.ens,
+  cache: new InMemoryCache(),
+});
+
+
+const ENS_ACCOUNT = `
+{
+  domains(where: { resolvedAddress: "{{ensAddress}}" }) {
+    id
+    name
+    labelName
+    labelhash
+    resolvedAddress {
+      id
+    }
+  }
+}
+`;
+
+/**
+* @summary writes name based on ENS settings
+* @param {object} data obtained from graph protocol
+* @param {string} publicAddress to parse
+*/
+const getENSName = (data, publicAddress) => {
+  console.log(data.domains);
+  if (data.domains.length > 0) {
+    return data.domains[0].name;
+  }
+  return shortenCryptoName(publicAddress);
+};
 
 /**
 * @summary renders a post in the timeline
 */
-export default class Account extends Component {
-  constructor(props) {
-    super(props);
+const AccountQuery = ({ publicAddress, width, height }) => {
+  let label;
+  if (publicAddress !== '0x0000000000000000000000000000000000000000') {
+    const { loading, error, data } = useQuery(gql(ENS_ACCOUNT.replace('{{ensAddress}}', publicAddress)));
 
-    this.state = {
-      image: makeBlockie(props.publicAddress),
-      url: `/address/${props.publicAddress}`,
-      label: shortenCryptoName(props.publicAddress),
-      width: props.width ? props.width : '24px',
-      height: props.height ? props.height : '24px',
-    };
+    if (loading) return null;
+    if (error) return `Error! ${error}`;
+
+    label = getENSName(data, publicAddress);
+  } else {
+    label = TAPi18n.__('moloch-unsponsored');
   }
 
-  render() {
-    return (
-      <div className="identity">
-        <div className="avatar-editor">
-          <img src={this.state.image} className="symbol profile-pic" role="presentation" style={{ width: this.state.width, height: this.state.height }} />
-          <div className="identity-peer">
-            <a href={this.state.url} title={this.props.publicAddress} className="identity-label identity-label-micro">
-              {this.state.label}
-            </a>
-          </div>
+  const image = makeBlockie(publicAddress);
+  const url = `/address/${publicAddress}`;
+  const finalWidth = width || '24px';
+  const finalHeight = height || '24px';
+
+  return (
+    <div className="identity">
+      <div className="avatar-editor">
+        <img src={image} className="symbol profile-pic" role="presentation" style={{ width: finalWidth, height: finalHeight }} />
+        <div className="identity-peer">
+          <a href={url} title={publicAddress} className="identity-label identity-label-micro">
+            {label}
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-Account.propTypes = {
+AccountQuery.propTypes = {
   publicAddress: PropTypes.string,
   width: PropTypes.string,
   height: PropTypes.string,
 };
+
+/**
+* @summary renders a post in the timeline
+*/
+const Account = (props) => {
+  return (
+    <ApolloProvider client={client}>
+      <AccountQuery publicAddress={props.publicAddress} width={props.width} height={props.height} />
+    </ApolloProvider>
+  );
+};
+
+Account.propTypes = AccountQuery.propTypes;
+
+export default Account;
