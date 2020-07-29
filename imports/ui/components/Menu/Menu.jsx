6dbf2b338ac2882b4100fb3aed4fa436bad62ddd
santisiri <santisiri@gmail.com>
2020-07-29 16:21:25 +0200

    display empty warning on sidebar menu

--- imports/ui/components/Menu/Menu.jsx
+++ imports/ui/components/Menu/Menu.jsx
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
+import { Router } from 'meteor/iron:router';
 
 import ApolloClient, { gql, InMemoryCache } from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
@@ -14,12 +15,7 @@ import DAO from '/imports/ui/components/DAO/DAO.jsx';
 
 const numeral = require('numeral');
 
-const client = new ApolloClient({
-  uri: Meteor.settings.public.graph.molochs,
-  cache: new InMemoryCache(),
-});
-
-const GET_MENU = `
+export const GET_MEMBERSHIPS = `
 {
   members(where: { memberAddress: "{{memberAddress}}" }) {
     id
@@ -49,8 +45,12 @@ const GET_MENU = `
     proposedToKick
   }
 }
-
 `;
+
+const client = new ApolloClient({
+  uri: Meteor.settings.public.graph.molochs,
+  cache: new InMemoryCache(),
+});
 
 /**
  * @summary style based on type of device
@@ -69,13 +69,13 @@ const _getMenuStyle = () => {
  */
 const _getProposalCount = (list, label) => {
   const now = parseInt(new Date().getTime() / 1000, 10);
-  const counts = [];
+  const counter = [];
 
   _.reduce(list, (memo, num) => {
     if (label === 'all') {
-      counts.push(num.submissions.length);
+      counter.push(num.submissions.length);
     }
-    counts.push(
+    counter.push(
       _.reduce(num.submissions, (iterator, proposal) => {
         switch (label) {
           case 'in-queue':
@@ -108,12 +108,18 @@ const _getProposalCount = (list, label) => {
       }, 0)
     );
   }, 0);
-  return _.reduce(counts, (memory, numerator) => { return parseInt(memory + numerator, 10); }, 0);
+  return _.reduce(counter, (memory, numerator) => { return parseInt(memory + numerator, 10); }, 0);
 };
 
+/**
+* @summary displays corresponding separator headline
+* @param {string} headline from TAPi18n dictionary
+* @param {string} account to parse in title
+* @return {string} with headline for separator
+*/
 const _getHeadline = (headline, account) => {
-  if (account === defaults.EMPTY) {
-    return TAPi18n.__(headline);
+  if (Router.current().url.replace(window.location.origin, '') === '/') {
+    return TAPi18n.__(`${headline}-sidebar`);
   }
   return TAPi18n.__(`${headline}-account`).replace('{{account}}', shortenCryptoName(account));
 };
@@ -122,39 +128,85 @@ const _getHeadline = (headline, account) => {
 * @summary displays the contents of a poll
 */
 const MenuQuery = ({ account }) => {
-  const { loading, error, data } = useQuery(gql(GET_MENU.replace('{{memberAddress}}', account)));
+  const atHome = (Router.current().url.replace(window.location.origin, '') === '/');
+  const hideEmpty = !atHome;
 
-  if (loading) {
-    return (
-      <div />
-    );
-  }
-  if (error) return `Error! ${error}`;
-
-  console.log(data);
-
-  const menuList = (
+  const defaultMenu = (
     <div>
-      <Item sharp hideEmpty label={`${TAPi18n.__('all')}`} score={_getProposalCount(data.members, 'all')} key={0} href="/" />
-      <Item sharp hideEmpty label={`${TAPi18n.__('in-queue')}`} score={_getProposalCount(data.members, 'in-queue')} key={1} href="/?status=queue" />
-      <Item sharp hideEmpty label={`${TAPi18n.__('voting-now')}`} score={_getProposalCount(data.members, 'voting-now')} key={2} href="/?status=voting" />
-      <Item sharp hideEmpty label={`${TAPi18n.__('grace-period')}`} score={_getProposalCount(data.members, 'grace-period')} key={3} href="/?status=grace" />
-      <Item sharp hideEmpty label={`${TAPi18n.__('ready-to-process')}`} score={_getProposalCount(data.members, 'ready-to-process')} key={4} href="/?status=ready" />
-      <Item sharp hideEmpty label={`${TAPi18n.__('guild-kicks')}`} score={_getProposalCount(data.members, 'guild-kicks')} key={9} href="/?status=kicked" />
-      <Item sharp hideEmpty label={`${TAPi18n.__('rejected')}`} score={_getProposalCount(data.members, 'rejected')} key={5} href="/?status=rejected" />
-      <Item sharp hideEmpty label={`${TAPi18n.__('approved')}`} score={_getProposalCount(data.members, 'approved')} key={6} href="/?status=approved" />
+      <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('all')}`} score={false} key={0} href="/" />
+      <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('in-queue')}`} score={false} key={1} href="/?status=queue" />
+      <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('voting-now')}`} score={false} key={2} href="/?status=voting" />
+      <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('grace-period')}`} score={false} key={3} href="/?status=grace" />
+      <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('ready-to-process')}`} score={false} key={4} href="/?status=ready" />
+      <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('guild-kicks')}`} score={false} key={9} href="/?status=kicked" />
+      <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('rejected')}`} score={false} key={5} href="/?status=rejected" />
+      <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('approved')}`} score={false} key={6} href="/?status=approved" />
     </div>
   );
 
-  const sorted = _.sortBy(data.members, (item) => { return (item.submissions.length * -1); });
+  console.log(`-0---Defualt maenu`);
+  console.log(defaultMenu);
+  console.log(_.where(defaultMenu.props.children));
 
-  const daoList = sorted.map((item, key) => {
-    return (
-      <Item key={key} href={`/dao/${item.moloch.id}`} score={item.submissions.length}>
-        <DAO publicAddress={item.moloch.id} width="16px" height="16px" format="plainText" />
-      </Item>
+  console.log(`account: ${account}`);
+
+  if (account !== defaults.EMPTY) {
+    const { loading, error, data } = useQuery(gql(GET_MEMBERSHIPS.replace('{{memberAddress}}', account)));
+
+    if (loading) {
+      return (
+        <div />
+      );
+    }
+    if (error) return `Error! ${error}`;
+
+    console.log(data);
+
+    const sorted = _.sortBy(data.members, (item) => { return (item.submissions.length * -1); });
+    const daoList = sorted.map((item, key) => {
+      return (
+        <Item key={key} href={`/dao/${item.moloch.id}`} score={item.submissions.length}>
+          <DAO publicAddress={item.moloch.id} width="16px" height="16px" format="plainText" />
+        </Item>
+      );
+    });
+
+    const menuList = (
+      <div>
+        <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('all')}`} score={(atHome) ? null : _getProposalCount(data.members, 'all')} key={0} href="/" />
+        <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('in-queue')}`} score={(atHome) ? null : _getProposalCount(data.members, 'in-queue')} key={1} href="/?status=queue" />
+        <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('voting-now')}`} score={(atHome) ? null : _getProposalCount(data.members, 'voting-now')} key={2} href="/?status=voting" />
+        <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('grace-period')}`} score={(atHome) ? null : _getProposalCount(data.members, 'grace-period')} key={3} href="/?status=grace" />
+        <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('ready-to-process')}`} score={(atHome) ? null : _getProposalCount(data.members, 'ready-to-process')} key={4} href="/?status=ready" />
+        <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('guild-kicks')}`} score={(atHome) ? null : _getProposalCount(data.members, 'guild-kicks')} key={9} href="/?status=kicked" />
+        <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('rejected')}`} score={(atHome) ? null : _getProposalCount(data.members, 'rejected')} key={5} href="/?status=rejected" />
+        <Item sharp hideEmpty={hideEmpty} label={`${TAPi18n.__('approved')}`} score={(atHome) ? null : _getProposalCount(data.members, 'approved')} key={6} href="/?status=approved" />
+      </div>
     );
-  });
+
+    return (
+      <div className="left">
+        <div className={_getMenuStyle()}>
+          <div className="menu">
+            <div className="separator">
+              {_getHeadline('proposals', account)}
+            </div>
+            {menuList}
+            <div className="separator">
+              {_getHeadline('memberships', account)}
+            </div>
+            {(daoList.length > 0) ?
+              daoList
+              :
+              <div className="empty">
+                {TAPi18n.__('no-memberships-found')}
+              </div>
+            }
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="left">
@@ -163,11 +215,7 @@ const MenuQuery = ({ account }) => {
           <div className="separator">
             {_getHeadline('proposals', account)}
           </div>
-          {menuList}
-          <div className="separator">
-            {_getHeadline('memberships', account)}
-          </div>
-          {daoList}
+          {defaultMenu}
         </div>
       </div>
     </div>
@@ -203,10 +251,18 @@ export default class Menu extends Component {
     }
   }
 
+  getContext() {
+    const current = Router.current().url.replace(window.location.origin, '');
+    if (current === '/') {
+      return this.state.accounts[0];
+    }
+    return Router.current().params.account;
+  }
+
   render() {
     return (
       <ApolloProvider client={client}>
-        <MenuQuery account={this.state.accounts[0]} />
+        <MenuQuery account={this.getContext()} />
       </ApolloProvider>
     );
   }
