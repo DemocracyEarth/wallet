339e2ced7711a165c3eec685c2b0ac55f24d3602
Brian Craig <briancraigok@gmail.com>
2020-08-21 10:24:55 -0300

    fix: small issue

--- src/contexts/Wallet/WalletContext.jsx
+++ src/contexts/Wallet/WalletContext.jsx
@@ -117,7 +117,7 @@ export const WalletContextProvider = ({ children }) => {
     }));
   }, [])
 
-  useEffect(() => onConnect, [])
+  useEffect(() => onConnect(), [])
 
   return <WalletContext.Provider value={{
     address: state.address,
