712ddb096a4b6ef72d132855bc9c2906c04e72ab
Santiago Siri <santisiri@gmail.com>
2020-09-14 23:14:18 +0200

    displays token icons

--- src/components/Token/Token.jsx
+++ src/components/Token/Token.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import BigNumber from 'bignumber.js/bignumber';
 import PropTypes from 'prop-types';
 
+import { config } from 'config'
+import web3 from 'web3';
+
 import 'styles/Dapp.css';
 
 const numeral = require('numeral');
@@ -13,9 +16,16 @@ const Token = (props) => {
   const zeroes = (!props.decimals) ? 0 : Number(props.decimals);
   const balance = numeral(new BigNumber(props.quantity).dividedBy(Math.pow(10, zeroes)).toNumber()).format('0,0.[00]');
 
+  console.log(`props.publicAddress: ${props.publicAddress}`);
+
   return (
     <div className="token">
       <div className="token-ticker">
+        {(props.publicAddress) ?
+          <img className="token-icon" src={`${config.web.icons.replace('{{publicAddress}}', web3.utils.toChecksumAddress(props.publicAddress))}`} alt="" />
+          :
+          null
+        }
         {props.symbol}
       </div>
       <div className="token-balance">
