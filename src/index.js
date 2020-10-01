3440619f1087f97257c7031704aeefd0930745ce
Santiago Siri <santisiri@gmail.com>
2020-10-01 13:02:42 +0200

    lazy query pattern to prevent memory leak

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import ReactGA from 'react-ga';
 import Dapp from 'components/Dapp/Dapp';
 import * as serviceWorker from './serviceWorker';
+import { config } from 'config';
+
+ReactGA.initialize(config.keys.analytics, { debug: true });
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
