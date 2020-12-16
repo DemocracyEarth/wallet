74b24ee5de9cfc69ecfc97708bbf560982f22265
pnestevez <pnestevez@gmail.com>
2020-12-16 15:53:41 -0300

    Validations

--- src/components/Proposal/Proposal.jsx
+++ src/components/Proposal/Proposal.jsx
@@ -8,9 +8,7 @@ import { defaults } from "lib/const";
 import { abiLibrary } from "../../lib/abi";
 import { Modal } from "react-bootstrap";
 import { Loader } from "rimble-ui";
-// import Switch from "react-input-switch";
-import "./style.css";
-
+// Forms
 import Details from "./Details"
 import Applicant from "./Applicant"
 import SharesRequested from "./SharesRequested"
@@ -18,16 +16,10 @@ import TributeOffered from "./TributeOffered"
 import PaymentRequested from "./PaymentRequested"
 import MemberToKick from "./MemberToKick"
 import TokenToWhitelist from "./TokenToWhitelist"
-
-// // Forms
-// import WhitelistProposal from "./WhitelistProposal";
-// import SubmitProposal from "./SubmitProposal";
-// import GuildKickProposal from "./GuildKickProposal";
-
+import "./style.css";
 // Functions
-import { walletError, notMember, _invalidAddress } from "components/Choice/messages";
+import {isAddress, isMember, submitProposal, submitWhitelistProposal, submitGuildKickProposal} from './utils'
 
-const Web3 = require("web3");
 const molochClient = new ApolloClient({
     uri: config.graph.moloch,
     cache: new InMemoryCache(),
@@ -47,13 +39,13 @@ const TYPES = [
 
 const INITIAL_STATE = {
     /* Proposal type */
-    isNewMember: false,
-    isFunding: true,
+    header: TYPES[0].title,
+    isNewMember: true,
+    isFunding: false,
     isTrade: false,
     isGuildKick: false,
     isWhitelist: false,
     isLoading: false,
-    header: 'Funding',
     /* Contract information */
     daoName: "",
     version: "",
@@ -75,172 +67,8 @@ const INITIAL_STATE = {
     link: "",
 };
 
-const isAddress = async (
-    address,
-) => {
-    const web3 = await new Web3("ws://localhost:8545");
-    return await web3.utils.isAddress(address)
-}
-
-const isMember = async (
-    memberAddress,
-    /*Contract information*/
-    library,
-    version,
-    contractAddress
-) => {
-    console.log('MEMBER ADDRESS: ', memberAddress)
-    const web3 = await new Web3("ws://localhost:8545");
-    
-    const isAddress = await web3.utils.isAddress(memberAddress)
-    if (!isAddress) return false
-    
-    const dao = await new web3.eth.Contract(
-        library[version === "2" ? "moloch2" : "moloch"],
-        contractAddress
-    );
-    
-    const response = await dao.methods
-        .members(web3.utils.toChecksumAddress(memberAddress))
-        .call({}, (err, res) => {
-            if (err) {
-                walletError(err);
-                return err;
-            }
-            return res;
-        });
-  
-    return response.exists;
-};
-
-const submitProposal = async (
-    /*Wallet information*/
-    user,
-    /*Contract information*/
-    library,
-    version,
-    address,
-    /*Proposal information*/
-    applicantAddress,
-    sharesRequested,
-    lootRequested,
-    tributeOffered,
-    tributeToken,
-    paymentRequested,
-    paymentToken,
-    details
-) => {
-    const web3 = new Web3("ws://localhost:8545");
-    const daoContract = await new web3.eth.Contract(
-        library[version === "2" ? "moloch2" : "moloch"],
-        address
-    );
-
-    // dao membership
-    if (version === "1" && !(await isMember(user, library, version))) {
-        return notMember();
-    }
-
-    const proposal = version === "2"
-        ? await daoContract.methods.submitProposal(
-            applicantAddress,
-            sharesRequested,
-            lootRequested,
-            tributeOffered,
-            tributeToken,
-            paymentRequested,
-            paymentToken,
-            details
-        )
-        : await daoContract.methods.submitProposal(
-            applicantAddress,
-            tributeToken,
-            sharesRequested,
-            `{"title":${details.title},"description":${details.description},"link":${details.link}}`
-        );
-
-    const estimatedGas = await proposal.estimateGas().then(price => price);
-
-    const receipt = await proposal
-        .send({ from: user, gas: estimatedGas })
-        .on("receipt", receipt => {
-            console.log("RECEIPT: ", receipt);
-            return receipt;
-        });
-
-    return receipt
-};
-
-const submitWhitelistProposal = async (
-    /*Wallet information*/
-    user,
-    /*Contract information*/
-    library,
-    version,
-    address,
-    /*Proposal information*/
-    tokenToWhitelist,
-    details
-) => {
-    const web3 = new Web3("ws://localhost:8545");
-    const daoContract = await new web3.eth.Contract(
-        library[version === "2" ? "moloch2" : "moloch"],
-        address
-    );
-
-    if (tokenToWhitelist === "0x0") return
-
-    const proposal = await daoContract.methods.submitWhitelistProposal(
-        tokenToWhitelist,
-        details
-    );
-    const estimatedGas = await proposal.estimateGas().then(price => price);
-
-    const receipt = await proposal
-        .send({ from: user, gas: estimatedGas })
-        .on("receipt", receipt => {
-        console.log("RECEIPT: ", receipt);
-        return receipt;
-        });
-
-    return receipt
-};
-
-const submitGuildKickProposal = async (
-    /*Wallet information*/
-    user,
-    /*Contract information*/
-    library,
-    version,
-    address,
-    /*Proposal information*/
-    memberToKick,
-    details
-) => {
-    const web3 = new Web3("ws://localhost:8545");
-    const daoContract = await new web3.eth.Contract(
-        library[version === "2" ? "moloch2" : "moloch"],
-        address
-    );
-
-    const proposal = await daoContract.methods.submitGuildKickProposal(
-        memberToKick,
-        details
-    );
-    const estimatedGas = await proposal.estimateGas().then(price => price);
-
-    const receipt = await proposal
-        .send({ from: user, gas: estimatedGas })
-        .on("receipt", receipt => {
-            console.log("RECEIPT: ", receipt);
-            return receipt;
-        });
-
-    return receipt
-};
-
 export default class Proposal extends Component {
-    state = { ... INITIAL_STATE };
+    state = { ...INITIAL_STATE };
 
     // Dao & tokens setting
     setDao = (address) => {
@@ -281,31 +109,30 @@ export default class Proposal extends Component {
         })
         .then((res) =>
             this.setState({
-                ERC20Tokens: res.data.tokens.filter(token => token != this.state.availableTokens)
+                ERC20Tokens: res.data.tokens.filter(token => token !== this.state.availableTokens)
             })
         );
     };
     
-    setType = (type) => {
-        TYPES.map(t => {
-            if(t.key == type && this.state[t.key] === false) this.setState({[type]: true, header: t.title})
-            if(t.key != type && this.state[t.key] === true) this.setState({[t.key]: false})
-            }
-        )
-    }
-
+    setType = (type) => TYPES.forEach(t => {
+        if(t.key === type && this.state[t.key] === false) this.setState({[type]: true, header: t.title})
+        if(t.key !== type && this.state[t.key] === true) this.setState({[t.key]: false})
+    })
+    
     // Component life cycle
     resetState = (e) => {
         if (e) e.preventDefault();
         this.setState({
             isLoading: false,
-            applicant: { address: this.props.user, validated: true },
+            applicant: {address: this.props.user, validated: true},
             sharesRequested: 0,
             lootRequested: 0,
             tributeOffered: 0,
-            tributeToken: defaults.EMPTY,
+            tributeToken: this.state.availableTokens[0].tokenAddress,
             paymentRequested: 0,
-            paymentToken: defaults.EMPTY,
+            paymentToken: this.state.availableTokens[0].tokenAddress,
+            tokenToWhitelist:defaults.EMPTY,
+            memberToKick: {address: defaults.EMPTY, validated: false},
             title: "",
             description: "",
             link: "",
@@ -336,24 +163,7 @@ export default class Proposal extends Component {
 
         this.setState({ isLoading: true });
 
-        const receipt = await submitProposal(
-            /*Wallet information*/
-            user,
-            /*Contract information*/
-            abiLibrary,
-            version,
-            address,
-            /*Proposal information*/
-            applicant.address,
-            sharesRequested,
-            lootRequested,
-            tributeOffered,
-            tributeToken,
-            paymentRequested,
-            paymentToken,
-            /* Details JSON */
-            { title, description, link }
-        );
+        await submitProposal(/*Wallet information*/ user, /*Contract information*/ abiLibrary, version, address, /*Proposal information*/ applicant.address, sharesRequested, lootRequested, tributeOffered, tributeToken, paymentRequested,paymentToken, /* Details JSON */ { title, description, link });
 
         this.resetState();
         this.props.onHide();
@@ -366,17 +176,7 @@ export default class Proposal extends Component {
         const { version, title, description, link, tokenToWhitelist } = this.state;
         const { user, address } = this.props;
 
-        const receipt = await submitWhitelistProposal(
-            /*Wallet information*/
-            user,
-            /*Contract information*/
-            abiLibrary,
-            version,
-            address,
-            /* Details JSON */
-            tokenToWhitelist,
-            { title, description, link }
-        );
+        await submitWhitelistProposal(/*Wallet information*/ user, /*Contract information*/ abiLibrary, version, address, /*Proposal information*/ tokenToWhitelist, /* Details JSON */ { title, description, link });
 
         this.resetState();
         this.props.onHide();
@@ -389,18 +189,7 @@ export default class Proposal extends Component {
         const { version, title, description, link, memberToKick } = this.state;
         const { user, address } = this.props;
 
-        const receipt = await submitGuildKickProposal(
-            /*Wallet information*/
-            user,
-            /*Contract information*/
-            abiLibrary,
-            version,
-            address,
-            /*Proposal information*/
-            memberToKick.address,
-            /* Details JSON */
-            { title, description, link }
-        );
+        await submitGuildKickProposal(/*Wallet information*/ user, /*Contract information*/ abiLibrary, version, address, /*Proposal information*/ memberToKick.address, /* Details JSON */ { title, description, link });
 
         this.resetState();
         this.props.onHide();
@@ -408,18 +197,22 @@ export default class Proposal extends Component {
 
     handleChanges = async(e) => {
         const name = e.target.name
-        if (name === 'aplicant' || name === 'memberToKick') {
-            const address = e.target.value
-            const validated = await name === 'memberToKick' ? isMember(address, abiLibrary, this.state.version, this.props.address) : isAddress(address)
-            this.setState({[name]: {address, validated}})
+        let value = e.target.value
+        let validated
+        if (name === 'applicant') {
+            validated = await isAddress(value) 
+            value = {address: value, validated} 
+        } else if (e.target.name === 'memberToKick') {
+            validated = await isMember(value, abiLibrary, this.state.version, this.props.address)
+            value = {address: value, validated} 
         } else {
-            const value = e.target.type === "number" && e.target.value < 0 ? 0 : e.target.value
-            this.setState({[name]: value})
+            value = e.target.type === "number" && e.target.value < 0 ? 0 : value
         }
+        this.setState({[name]: value})
     }
 
   render() {
-      const { isNewMember, isFunding, isTrade, isGuildKick, isWhitelist, header, availableTokens, ERC20Tokens, applicant, sharesRequested, lootRequested,tributeOffered, tributeToken, paymentRequested, paymentToken, tokenToWhitelist, memberToKick, title, description, link } = this.state
+      const { isNewMember, isFunding, isTrade, isGuildKick, isWhitelist, header, version, availableTokens, ERC20Tokens, applicant, sharesRequested, lootRequested,tributeOffered, tributeToken, paymentRequested, paymentToken, tokenToWhitelist, memberToKick, title, description, link } = this.state
       const { show, onHide } = this.props
 
     return (
@@ -427,17 +220,18 @@ export default class Proposal extends Component {
         <Modal.Body className="modal">
           <div className="container">
             <div className="header">
-              <img src="/static/media/flag.44f0a516.svg" />
+              <img src="/static/media/flag.44f0a516.svg" alt="flag"/>
               <h4>{this.state.daoName}</h4>
             </div>
             <div className="formContainer">
                 <div className="title">
-                <h2>{`New ${header} Proposal`}</h2>
+                    <h2>{`New ${header} Proposal`}<span>{` (v${version})`}</span></h2>
                 </div>
                 <div className="switch">
                     {TYPES.map((t,i) => 
                         <button
                             key={i}
+                            disabled={(t.key === 'isGuildKick' || t.key  === 'isWhitelist') && version === '1'}
                             className={`switchButton ${this.state[t.key] ? 'switched' : null}`}
                             onClick={() => this.setType(t.key)}
                         >{t.title}</button>)}
@@ -468,7 +262,6 @@ export default class Proposal extends Component {
                         ?<TokenToWhitelist tokenToWhitelist={tokenToWhitelist} ERC20Tokens={ERC20Tokens} handleChanges ={this.handleChanges}/>
                         : null
                     }
-                    
                     <div className="section end">
                         <button className="submit clear" onClick={this.resetState}>
                             Clear
@@ -491,50 +284,6 @@ export default class Proposal extends Component {
                 </form>
             </div>
           </div>
-            {/* <div className="switch">
-              <h2>Whitelist Proposal?</h2>
-              <Switch
-                value={this.state.whitelist}
-                on="Yes"
-                off="No"
-                onChange={(whitelist) => {
-                  this.setState({ whitelist, guildkick: "No" });
-                }}
-              />
-
-              <h2>GuildKick Proposal?</h2>
-              <Switch
-                value={this.state.guildkick}
-                on="Yes"
-                off="No"
-                onChange={(guildkick) => this.setState({ guildkick, whitelist: "No" })}
-              />
-            </div>
-
-            {this.state.whitelist === "Yes" ? (
-              <WhitelistProposal
-                state={this.state}
-                handleChanges={this.handleChanges}
-                handleSubmit={this.handleSubmitWhitelist}
-              />
-            ) : null}
-
-            {this.state.guildkick === "Yes" ? (
-              <GuildKickProposal
-                state={this.state}
-                handleChanges={this.handleChanges}
-                handleSubmit={this.handleSubmitKickProposal}
-              />
-            ) : null}
-
-            {this.state.whitelist === "No" && this.state.guildkick === "No" ? (
-              <SubmitProposal
-                state={this.state}
-                handleChanges={this.handleChanges}
-                handleSubmit={this.handleSubmit}
-                resetState={this.resetState}
-              />
-            ) : null} */}
         </Modal.Body>
       </Modal>
     );
--- src/components/Proposal/utils.js
+++ src/components/Proposal/utils.js
@@ -1,8 +1,112 @@
-// COMPOSE PROPOSAL
-export const composeProposal = async (
-  /* Contract */ contract,
+/* IMPORTS */
+// Components
+import logo from 'images/logo.png';
+import i18n from 'i18n';
+// Functions
+import { walletError, notMember} from "components/Choice/messages";
+
+const Web3 = require("web3");
+
+///////////////////////// UX UTILS /////////////////////////
+
+const showModal = () => {
+  window.modal = {
+      icon: logo,
+      title: i18n.t('Proposal submitted'),
+      message: i18n.t('Your proposal was successfully submitted'),
+      cancelLabel: i18n.t('close'),
+      mode: 'ALERT'
+  }
+  window.showModal.value = true;
+}
+
+///////////////////////// VALIDATIONS /////////////////////////
+
+export const isAddress = async (
+  address,
+) => {
+  const web3 = await new Web3("ws://localhost:8545");
+  return await web3.utils.isAddress(address)
+}
+
+export const isMember = async (
+  memberAddress,
+  /*Contract information*/
+  library,
   version,
-  /* Proposal arguments */ applicant,
+  contractAddress
+) => {
+  const web3 = await new Web3("ws://localhost:8545");
+  
+  const isAddress = await web3.utils.isAddress(memberAddress)
+  if (!isAddress) return false
+
+  console.log('NO ES ADDRESS')
+  
+  const dao = await new web3.eth.Contract(
+      library[version === "2" ? "moloch2" : "moloch"],
+      contractAddress
+  );
+
+  console.log(dao.methods)
+  
+  const response = await dao.methods.members(web3.utils.toChecksumAddress(memberAddress))
+      .call({}, (err, res) => {
+        console.log('ADDRESS', memberAddress)
+          if (err) {
+              walletError(err);
+              return err;
+          }
+          return res;
+      });
+  console.log('AY!', response)
+  return response.exists;
+};
+
+///////////////////////// SUBMITTING UTILS /////////////////////////
+
+const getDao = async (library, version, address) => {
+  const web3 = new Web3("ws://localhost:8545");
+  const dao = await new web3.eth.Contract(
+    library[version === "2" ? "moloch2" : "moloch"],
+    address
+  )
+  return dao
+}
+
+const estimateGas = async (proposal) => {
+  let gas
+  try{
+      gas = await proposal.estimateGas().then(gas=> gas)
+  } catch (err){
+      console.log('ERROR: ', err)
+  }
+  return gas
+} 
+
+const getReceipt = async (proposal, user, estimatedGas) => {
+  let receipt
+  try{
+      receipt = await proposal.send({ from: user, gas: estimatedGas })
+      .on('receipt', receipt => { showModal(); return receipt })
+  } catch (err) {
+      walletError(err);
+      receipt = err;
+  }
+  return receipt
+}
+
+///////////////////////// SUBMITTING FUNCTIONS /////////////////////////
+
+export const submitProposal = async (
+  /*Wallet information*/
+  user,
+  /*Contract information*/
+  library,
+  version,
+  address,
+  /*Proposal information*/
+  applicantAddress,
   sharesRequested,
   lootRequested,
   tributeOffered,
@@ -11,24 +115,84 @@ export const composeProposal = async (
   paymentToken,
   details
 ) => {
-  if (version === "1")
-    return await contract.methods.submitProposal(
-      applicant,
-      tributeToken,
-      sharesRequested,
-      `{"title":${details.title},"description":${details.description},"link":${details.link}}`
-    );
-  if (version === "2")
-    return await contract.methods.submitProposal(
-      applicant,
-      sharesRequested,
-      lootRequested,
-      tributeOffered,
-      tributeToken,
-      paymentRequested,
-      paymentToken,
-      details
-    );
+  const dao = await getDao(library, version, address);
 
-  return false;
+  // validations
+  if (version === "1" && !(await isMember(user, library, version))) {
+      return notMember();
+  }
+
+  const proposal = version === "2"
+      ? await dao.methods.submitProposal(
+          applicantAddress,
+          sharesRequested,
+          lootRequested,
+          tributeOffered,
+          tributeToken,
+          paymentRequested,
+          paymentToken,
+          details
+      )
+      : await dao.methods.submitProposal(
+          applicantAddress,
+          tributeToken,
+          sharesRequested,
+          `{"title":${details.title},"description":${details.description},"link":${details.link}}`
+      );
+  const estimatedGas = await estimateGas(proposal)
+  const receipt =  await getReceipt(proposal, user, estimatedGas)
+
+  return receipt
+};
+
+export const submitWhitelistProposal = async (
+  /*Wallet information*/
+  user,
+  /*Contract information*/
+  library,
+  version,
+  address,
+  /*Proposal information*/
+  tokenToWhitelist,
+  details
+) => {
+  const dao = await getDao(library, version, address);
+
+  // validations
+  if (tokenToWhitelist === "0x0") return
+
+  const proposal = await dao.methods.submitWhitelistProposal(
+      tokenToWhitelist,
+      details
+  );
+  const estimatedGas = await estimateGas(proposal)
+  const receipt =  await getReceipt(proposal, user, estimatedGas)
+
+  return receipt
+};
+
+export const submitGuildKickProposal = async (
+  /*Wallet information*/
+  user,
+  /*Contract information*/
+  library,
+  version,
+  address,
+  /*Proposal information*/
+  memberToKick,
+  details
+) => {
+  const dao = await getDao(library, version, address);
+
+  // validations
+  if (!isAddress(memberToKick)) return
+
+  const proposal = await dao.methods.submitGuildKickProposal(
+      memberToKick,
+      details
+  );
+  const estimatedGas = await estimateGas(proposal)
+  const receipt =  await getReceipt(proposal, user, estimatedGas)
+
+  return receipt
 };
