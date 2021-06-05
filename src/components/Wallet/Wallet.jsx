2b359dda9c84dd8e86a31a07f1df46df842b9b0c
Santiago Siri <santisiri@gmail.com>
2021-06-05 15:58:43 +0200

    atm view

--- src/components/Wallet/Wallet.jsx
+++ src/components/Wallet/Wallet.jsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Button from '@material-ui/core/Button';
+
 import i18n from 'i18n';
 
 import 'styles/Dapp.css';
+import 'styles/material.css';
 
 /**
 * @summary renders a post in the timeline
@@ -20,9 +22,9 @@ export default class Wallet extends Component {
   render() {
     return (
       <>
-        <Button>{i18n.t('approve')}</Button>
-        <Button>{i18n.t('deposit')}</Button>
-        <Button>{i18n.t('withdraw')}</Button>
+        <Button className="wallet-button" color="primary" variant="contained">{i18n.t('approve')}</Button>
+        <Button className="wallet-button" variant="contained" disabled>{i18n.t('deposit')}</Button>
+        <Button className="wallet-button" variant="contained" disabled>{i18n.t('withdraw')}</Button>
       </>
     );
   }
