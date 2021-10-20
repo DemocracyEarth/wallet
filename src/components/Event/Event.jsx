c43ec53c3d53709847335e04938641c5c22a22d7
Matt <github.27jtz@aleeas.com>
2021-10-20 10:56:10 -0300

    fix: compile warnings

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -25,13 +25,13 @@ const Web3 = require('web3');
 function ordinal_suffix_of(i) {
   var j = i % 10,
     k = i % 100;
-  if (j == 1 && k != 11) {
+  if (Number(j) === 1 && Number(k) !== 11) {
     return i + "st";
   }
-  if (j == 2 && k != 12) {
+  if (Number(j) === 2 && Number(k) !== 12) {
     return i + "nd";
   }
-  if (j == 3 && k != 13) {
+  if (Number(j) === 3 && Number(k) !== 13) {
     return i + "rd";
   }
   return i + "th";
